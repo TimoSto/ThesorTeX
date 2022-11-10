c40de23bdbb9268ebe50974b750f66530cc042a6
Timo Stovermann <tsto@d-velop.de>
2022-11-10 18:40:23 +0100

    favicon not perfect

--- webclient/src/pages/index/Index.vue
+++ webclient/src/pages/index/Index.vue
@@ -1,6 +1,12 @@
 <template>
   <v-app>
-
+    <v-img
+        src="../../../public/logo.svg"
+        width="150"
+        height="150"
+        style="max-height: 150px"
+        contain
+    />
   </v-app>
 </template>
 
