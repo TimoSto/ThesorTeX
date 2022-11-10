cfc1057610aa0e5c403c52ae404c01070c1f7971
Timo Stovermann <tsto@d-velop.de>
2022-11-10 19:16:48 +0100

    logo component

--- webclient/src/pages/index/Index.vue
+++ webclient/src/pages/index/Index.vue
@@ -1,21 +1,20 @@
 <template>
   <v-app>
-    <v-img
-        src="../../../public/logo.svg"
-        width="150"
-        height="150"
-        style="max-height: 150px"
-        contain
+    <LogoSVG
+        fill="black"
+        style="width: 150px"
     />
+    <h2>ThesorTeX</h2>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import LogoSVG from "../../components/LogoSVG.vue";
 
 export default Vue.extend({
   name: 'App',
-
+  components: {LogoSVG},
   data: () => ({
     //
   }),
