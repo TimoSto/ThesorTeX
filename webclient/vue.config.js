68f9fd071f34871a06220884bc87405653877099
Timo Stovermann <tsto@d-velop.de>
2022-11-10 17:11:40 +0100

    auth page

--- webclient/vue.config.js
+++ webclient/vue.config.js
@@ -2,5 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pages: {
+    auth: './src/auth/main.ts'
+  }
 })
