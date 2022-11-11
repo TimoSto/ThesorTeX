ce72740875d470a3807b52a37463d999b56d824a
Timo Stovermann <tsto@d-velop.de>
2022-11-11 09:02:28 +0100

    bar and content

---
+++ webclient/src/components/ContentBelowBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-sheet
+      style="overflow-y: auto;"
+      :style="`
+        height: calc(100% - ${barHeight}px);
+        margin-top: ${barHeight}px;
+        padding: ${paddingY}px ${paddingX}px;
+        `"
+  >
+    <slot></slot>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: "ContentBelowBar",
+  props: [
+      'barHeight',
+      'paddingX',
+      'paddingY'
+  ]
+}
+</script>
+
+<style scoped>
+
+</style>
--- webclient/src/pages/index/Index.vue
+++ webclient/src/pages/index/Index.vue
@@ -1,20 +1,42 @@
 <template>
   <v-app>
-    <LogoSVG
-        fill="black"
-        style="width: 150px"
-    />
-    <h2>ThesorTeX</h2>
+    <v-app-bar
+      fixed
+      elevation="0"
+      color="background"
+    >
+      <LogoSVG
+          fill="rgba(0, 136, 52, 0.8)"
+          style="height: 35px"
+          :show-title="true"
+          font-color="rgba(0,0,0,0.7)"
+      />
+
+      <v-spacer />
+
+      <v-btn text color="primary">
+        Anmelden
+      </v-btn>
+
+    </v-app-bar>
+    <ContentBelowBar
+      bar-height="64"
+      padding-x="16"
+      padding-y="0"
+    >
+      hallo
+    </ContentBelowBar>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import LogoSVG from "../../components/LogoSVG.vue";
+import ContentBelowBar from "../../components/ContentBelowBar.vue";
 
 export default Vue.extend({
   name: 'App',
-  components: {LogoSVG},
+  components: {ContentBelowBar, LogoSVG},
   data: () => ({
     //
   }),
