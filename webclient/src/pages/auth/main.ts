44a06a6c19e0049d86b00f03b960c409ea764c48
Timo Stovermann <tsto@d-velop.de>
2022-11-13 10:20:44 +0100

    rm unused

--- webclient/src/pages/auth/main.ts
+++ webclient/src/pages/auth/main.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './Auth.vue'
 import router from './router'
-import store from '../../store'
 import vuetify from '../../plugins/vuetify'
 import CreateI18n from "@/plugins/i18n";
 import {GermanTranslations} from "@/pages/auth/i18n/German";
@@ -13,7 +12,6 @@ const i18n = CreateI18n(GermanTranslations, EnglishTranslations)
 
 new Vue({
   router,
-  store,
   vuetify,
   i18n,
   render: h => h(App)
--- webclient/src/pages/index/main.ts
+++ webclient/src/pages/index/main.ts
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import App from './Index.vue'
-import router from '../../router'
-import store from '../../store'
 import vuetify from '../../plugins/vuetify'
 import CreateI18n from "@/plugins/i18n";
 import {GermanTranslations} from "@/pages/index/i18n/German";
@@ -12,8 +10,6 @@ Vue.config.productionTip = false
 const i18n = CreateI18n(GermanTranslations, EnglishTranslations)
 
 new Vue({
-  router,
-  store,
   vuetify,
   i18n,
   render: h => h(App)
