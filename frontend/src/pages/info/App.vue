7ab05572cf330e48e376226f1961ee0f1068271b
Timo Stovermann <tsto@d-velop.de>
2022-11-24 15:48:58 +0100

    setup scss

--- frontend/src/pages/info/App.vue
+++ frontend/src/pages/info/App.vue
@@ -1,6 +1,9 @@
 <template>
   <v-app>
-    <v-main>
+    <v-main class="test">
+      <div class="t2">
+
+      </div>
       <HelloWorld />
     </v-main>
   </v-app>
@@ -9,3 +12,14 @@
 <script setup lang="ts">
   import HelloWorld from '../../components/HelloWorld.vue'
 </script>
+
+<style lang="scss" scoped>
+.test {
+  background-color: red;
+  & .t2 {
+    width: 50px;
+    height: 50px;
+    background-color: darkblue;
+  }
+}
+</style>
