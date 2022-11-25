fd1e0a7b889c24c15881e80c3d4c3345de0e50c6
Timo Stovermann <tsto@d-velop.de>
2022-11-25 17:01:03 +0100

    rm unnecessary stuff

--- frontend/src/pages/app/App.vue
+++ frontend/src/pages/app/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-layout>
+  <v-app>
     <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
 
     <v-app-bar
@@ -12,10 +12,6 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn variant="text" icon="mdi-magnify"></v-btn>
-
-      <v-btn variant="text" icon="mdi-filter"></v-btn>
-
       <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
     </v-app-bar>
 
@@ -23,48 +19,22 @@
       permanent
       :rail="!drawer"
     >
-      <v-list
-        :items="items"
-      ></v-list>
+
     </v-navigation-drawer>
 
     <v-main>
-      <v-card-text>
-        The navigation drawer will appear from the bottom on smaller size screens.
-      </v-card-text>
+
     </v-main>
-  </v-layout>
+  </v-app>
 </template>
 
 <script>
 export default {
   data: () => ({
     drawer: false,
-    group: null,
-    items: [
-      {
-        title: 'Foo',
-        value: 'foo',
-      },
-      {
-        title: 'Bar',
-        value: 'bar',
-      },
-      {
-        title: 'Fizz',
-        value: 'fizz',
-      },
-      {
-        title: 'Buzz',
-        value: 'buzz',
-      },
-    ],
   }),
 
   watch: {
-    group () {
-      this.drawer = false
-    },
   },
 }
 </script>
