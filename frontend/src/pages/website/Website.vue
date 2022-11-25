7a37b32a7b8311c61ae4073767fd11f91b72a236
Timo Stovermann <tsto@d-velop.de>
2022-11-25 12:29:14 +0100

    wrap in v-app

--- frontend/src/pages/website/Website.vue
+++ frontend/src/pages/website/Website.vue
@@ -1,45 +1,47 @@
 <template>
-  <v-layout>
-    <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
+  <v-app>
+    <v-layout>
+      <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
 
-    <AppbarContent
-      bar-color="background"
-      :level="1"
-    >
-      <template v-slot:bar>
-        <LogoSVG
-          fill="rgba(0, 136, 52, 0.8)"
-          style="height: 35px; margin-left: 20px;"
-          :show-title="true"
-          font-color="rgba(0,0,0,0.7)"
-        />
+      <AppbarContent
+        bar-color="background"
+        :level="1"
+      >
+        <template v-slot:bar>
+          <LogoSVG
+            fill="rgba(0, 136, 52, 0.8)"
+            style="height: 35px; margin-left: 20px;"
+            :show-title="true"
+            font-color="rgba(0,0,0,0.7)"
+          />
 
-        <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
 
-        <v-btn
-          variant="text"
-          color="primary"
-        >Download</v-btn>
-      </template>
-      <template v-slot:content>
+          <v-btn
+            variant="text"
+            color="primary"
+          >Download</v-btn>
+        </template>
+        <template v-slot:content>
 
-        <ContentAndImage
-          image-side="right">
-          <template v-slot:left>
-            <p class="tile_heading">{{t(i18nKeys.Template.Title)}}</p>
-            <p class="tile_text" v-html="t(i18nKeys.Template.Text)"></p>
-            <a href="/static/downloads/cvTemplate.zip" download="cvTemplate.zip" style="text-decoration: none">
-              <v-btn block color="primary">{{t(i18nKeys.Common.Download)}}</v-btn>
-            </a>
-          </template>
-          <template v-slot:right>
-            hallos
-          </template>
-        </ContentAndImage>
+          <ContentAndImage
+            image-side="right">
+            <template v-slot:left>
+              <p class="tile_heading">{{t(i18nKeys.Template.Title)}}</p>
+              <p class="tile_text" v-html="t(i18nKeys.Template.Text)"></p>
+              <a href="/static/downloads/cvTemplate.zip" download="cvTemplate.zip" style="text-decoration: none">
+                <v-btn block color="primary">{{t(i18nKeys.Common.Download)}}</v-btn>
+              </a>
+            </template>
+            <template v-slot:right>
+              hallos
+            </template>
+          </ContentAndImage>
 
-      </template>
-    </AppbarContent>
-  </v-layout>
+        </template>
+      </AppbarContent>
+    </v-layout>
+  </v-app>
 </template>
 
 <script setup lang="ts">
@@ -48,22 +50,8 @@ import AppbarContent from "@/components/AppbarContent";
 import ContentAndImage from "@/components/ContentAndImage.vue";
 import {useI18n} from "vue-i18n";
 import {i18nKeys} from "./i18n/keys";
-import {ThemeInstance, useTheme} from "vuetify";
-import {ref, watch, watchEffect} from "vue";
 
 const { t } = useI18n()
-
-const themeName = ref("light");
-
-const theme = useTheme();
-
-watch(themeName, () => {
-  theme.global.name.value = themeName.value;
-})
-
-setTimeout(() => {
-  themeName.value = "dark";
-}, 2500)
 
 </script>
 
