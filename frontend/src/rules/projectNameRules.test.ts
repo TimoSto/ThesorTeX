ff8a457f1e344e51aa1c7cb8fde71dfc9558cd24
Timo Stovermann <tsto@d-velop.de>
2022-11-30 11:29:37 +0100

    add testcases for projectNameRules

--- frontend/src/rules/projectNameRules.test.ts
+++ frontend/src/rules/projectNameRules.test.ts
@@ -2,7 +2,13 @@ import getProjectNameRules from "@/rules/projectNameRules";
 import {i18nKeys} from "@/pages/app/i18n/keys";
 
 describe('ProjectNameRules', () => {
-  it('should throw error', () => {
+  it('should throw error on space', () => {
     expect(getProjectNameRules([])[0]('t t')).toEqual(i18nKeys.Rules.NoSpaces)
+  });
+  it('should throw error on existing', () => {
+    expect(getProjectNameRules(['t'])[0]('t')).toEqual(i18nKeys.Rules.ProjectAlreadyExists)
+  });
+  it('should throw error on space', () => {
+    expect(getProjectNameRules(['t'])[0]('tt')).toBe(true);
   })
 })
