416cf90b1d02bc0de1817c2fcd93213477d568c7
Timo Stovermann <tsto@d-velop.de>
2022-12-04 15:02:40 +0100

    App: Unit test for errorSuccessStore

---
+++ frontend/src/pages/app/stores/errorSuccessStore.test.ts
@@ -0,0 +1,35 @@
+import { setActivePinia, createPinia } from 'pinia'
+import {useErrorSuccessStore} from "@/pages/app/stores/errorSuccessStore";
+
+describe('ErrorSuccessStore', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  });
+
+  describe('hanldeResponse', () => {
+    it('set error', () => {
+      const store = useErrorSuccessStore();
+      expect(store.errorMessage).toEqual('');
+      expect(store.successMessage).toEqual('');
+      store.handleResponse(false, 'test 1', 'error1')
+      expect(store.errorMessage).toEqual('error1');
+      expect(store.successMessage).toEqual('');
+    })
+    it('set success', () => {
+      const store = useErrorSuccessStore();
+      expect(store.errorMessage).toEqual('');
+      expect(store.successMessage).toEqual('');
+      store.handleResponse(true, 'test 1', 'error1')
+      expect(store.errorMessage).toEqual('');
+      expect(store.successMessage).toEqual('test 1');
+    })
+    it('clean', () => {
+      const store = useErrorSuccessStore();
+      store.errorMessage = 'test';
+      store.successMessage = 'test2';
+      store.clean();
+      expect(store.errorMessage).toEqual('');
+      expect(store.successMessage).toEqual('');
+    })
+  })
+})
