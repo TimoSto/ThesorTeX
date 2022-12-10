7b8dcdfae708d2940855336720623e2a5bac8397
Timo Stovermann <tsto@d-velop.de>
2022-12-10 17:06:38 +0100

    App: Generate csv for entries /tested)

---
+++ services/app/internal/bib_entries/entries_to_csv.go
@@ -0,0 +1,26 @@
+package bib_entries
+
+import (
+	"strings"
+
+	"github.com/TimoSto/ThesorTeX/services/app/internal/database"
+)
+
+func GenerateCsvForEntries(entries []database.BibEntry) []byte {
+	file := "key;type;a;b;c;d;e;f;g;h;i;j;k;l;m;n;o;p;q;r;s;t;u;v;w;x;y;z;\n"
+	for _, entry := range entries {
+		file += entry.Key + ";" + entry.Category + ";"
+		for i := 0; i < 26; i++ {
+			if i < len(entry.Fields) {
+				//because json converts & into amp;
+				strToAdd := strings.Replace(entry.Fields[i], "amp;", "", -1)
+				file += strToAdd
+			}
+			file += ";"
+		}
+		file += "\n"
+	}
+	file += "empty;empty;a;b;c;d;e;f;g;h;i;j;k;l;m;n;o;p;q;r;s;t;u;v;w;x;y;z;\n"
+
+	return []byte(file)
+}
