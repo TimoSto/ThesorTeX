88174a7907da62ee8528a3f7857fd2b2bf4eea69
Timo Stovermann <tsto@d-velop.de>
2022-12-11 10:37:38 +0100

    App: Setup category editor tables and expandables

--- frontend/src/pages/app/views/CategoryEditor.vue
+++ frontend/src/pages/app/views/CategoryEditor.vue
@@ -15,12 +15,56 @@
       </v-toolbar-title>
     </template>
 
-    <template #content></template>
+    <template #content>
+      <v-expansion-panels
+        multiple
+        variant="accordion"
+        :model-value="[0, 1, 2]"
+      >
+        <v-expansion-panel>
+          <v-expansion-panel-title>
+            Allgemein
+          </v-expansion-panel-title>
+          <v-expansion-panel-text>
+            <ResponsiveTable
+              :headers="generalHeaders"
+              :rows="generalRows"
+            >
+
+            </ResponsiveTable>
+          </v-expansion-panel-text>
+        </v-expansion-panel>
+        <v-expansion-panel>
+          <v-expansion-panel-title>
+            Literaturverzeichnis
+          </v-expansion-panel-title>
+          <v-expansion-panel-text>
+            <ResponsiveTable
+              :headers="bibHeaders"
+              :rows="bibFields"
+            ></ResponsiveTable>
+          </v-expansion-panel-text>
+        </v-expansion-panel>
+        <v-expansion-panel>
+          <v-expansion-panel-title>
+            Zitate
+          </v-expansion-panel-title>
+          <v-expansion-panel-text>
+            <ResponsiveTable
+              :headers="bibHeaders"
+              :rows="citeFields"
+            ></ResponsiveTable>
+          </v-expansion-panel-text>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </template>
   </AppbarContent>
 </template>
 
 <script setup lang="ts">
 import AppbarContent from "../../../components/AppbarContent.vue";
+import ResponsiveTable, {ResponsiveTableCell, ResponsiveTableHeaderCell} from "../../../components/ResponsiveTable.vue";
+import {ref} from "vue";
 
 //globals
 const emit = defineEmits(['navBack']);
@@ -31,7 +75,153 @@ const props = defineProps({
     type: String,
     required: true
   }
-})
+});
+
+// data
+const generalHeaders: ResponsiveTableHeaderCell[] = [
+  {
+    width: '40%',
+    minWidth: '',
+    content: 'Attribut',
+    icon: '',
+    hideUnder: -1,
+    event: ''
+  },
+  {
+    width: '60%',
+    minWidth: '',
+    content: 'Wert',
+    icon: '',
+    hideUnder: -1,
+    event: ''
+  }
+];
+
+const bibHeaders: ResponsiveTableHeaderCell[] = [
+  {
+    width: '16%',
+    minWidth: '',
+    content: 'Feld',
+    icon: '',
+    hideUnder: -1,
+    event: ''
+  },
+  {
+    width: '16%',
+    minWidth: '',
+    content: 'Stil',
+    icon: '',
+    hideUnder: -1,
+    event: ''
+  },
+  {
+    width: '16%',
+    minWidth: '',
+    content: 'Prefix',
+    icon: '',
+    hideUnder: -1,
+    event: ''
+  },
+  {
+    width: '16%',
+    minWidth: '',
+    content: 'Suffix',
+    icon: '',
+    hideUnder: -1,
+    event: ''
+  },
+  {
+    width: '16%',
+    minWidth: '',
+    content: 'Formatiert',
+    icon: '',
+    hideUnder: -1,
+    event: ''
+  },
+  {
+    width: '16%',
+    minWidth: '',
+    content: 'Citavi Attribute',
+    icon: '',
+    hideUnder: -1,
+    event: ''
+  },
+  {
+    width: '48px',
+    minWidth: '',
+    content: '',
+    icon: '',
+    hideUnder: -1,
+    event: ''
+  },
+];
+
+const generalRows: ResponsiveTableCell[][] = [
+  [
+    {
+      content: 'Name',
+      icon: '',
+      event: '',
+      hideUnder: -1
+    },
+    {
+      content: '',
+      icon: '',
+      event: '',
+      hideUnder: -1,
+      slot: true
+    },
+  ],
+  [
+    {
+      content: 'Kategorie',
+      icon: '',
+      event: '',
+      hideUnder: -1
+    },
+    {
+      content: '',
+      icon: '',
+      event: '',
+      hideUnder: -1,
+      slot: true
+    },
+  ],
+  [
+    {
+      content: 'Citavi Kategorie',
+      icon: '',
+      event: '',
+      hideUnder: -1
+    },
+    {
+      content: '',
+      icon: '',
+      event: '',
+      hideUnder: -1,
+      slot: true
+    },
+  ],
+  [
+    {
+      content: 'Citavi Filter',
+      icon: '',
+      event: '',
+      hideUnder: -1
+    },
+    {
+      content: '',
+      icon: '',
+      event: '',
+      hideUnder: -1,
+      slot: true
+    },
+  ],
+];
+
+const bibFields = ref([] as ResponsiveTableCell[][]);
+
+const citeFields = ref([] as ResponsiveTableCell[][]);
 </script>
 
 <style scoped>
