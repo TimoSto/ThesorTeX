31548a65dc6aa6cd4669d3f7ffc952bb33341bce
Timo Stovermann <tsto@d-velop.de>
2022-12-23 10:17:00 +0100

    Testing: Test menu open via keyboard

--- testing/playwright/src/features/app/configDialog.feature
+++ testing/playwright/src/features/app/configDialog.feature
@@ -1,8 +1,15 @@
 Feature: Display the config dialog
   The config dialog is globally accessible and contains the config for the app
 
-  Scenario: The config dialog is accessible via mouse clicks
+  Scenario: The config dialog is accessible via mouse navigation
     Given the page "http://localhost:8448/app/" was opened
     When I click the tree dot button
+    Then I expect to see a menu in the top right corner
+    Then I expect the three dot menu to contain the following entries "Settings"
+
+  Scenario: The config dialog is accessible via keyboard navigation
+    Given the page "http://localhost:8448/app/" was opened
+    When I press the tab key 1 times
+    And I press the enter key
     Then I expect to see a menu in the top right corner
     Then I expect the three dot menu to contain the following entries "Settings"
--- testing/playwright/src/step_definitions/global/browser_steps.ts
+++ testing/playwright/src/step_definitions/global/browser_steps.ts
@@ -2,18 +2,32 @@ import { Given, When, Then } from "@cucumber/cucumber";
 import assert from "assert";
 import { OurWorld } from "../../support/OurWorld";
 import {expect} from "@playwright/test";
-// Using a cucumber expression
+
 Given("the page {string} was opened", async function (this: OurWorld, url: string) {
     // Use the page instance from the World instance to navigate
     await this.page.goto(url);
 });
-// Using a regular expression
+
+When("I press the tab key {int} times", async function (this: OurWorld, n: number) {
+    while (n > 0) {
+        await this.page.keyboard.press("Tab");
+        n--;
+    }
+});
+
+When("I press the enter key", async function (this: OurWorld) {
+    await this.page.keyboard.press("Enter");
+});
+
 When("I click the tree dot button", async function (this: OurWorld) {
     // Scroll to the link...
     const element = this.page.locator(".mdi-dots-vertical >> xpath=../..");
     await expect(element).toBeVisible();
     await element.click();
 });
+
+
+
 Then("I expect to see a menu in the top right corner", async function (this: OurWorld) {
     const menuElement = this.page.locator(".v-overlay__content:visible")
     await expect(menuElement).toBeVisible();
