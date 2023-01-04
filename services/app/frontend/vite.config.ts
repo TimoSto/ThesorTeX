edb59166b14dc682cebcb7f528519d9329441534
Timo Stovermann <tsto@d-velop.de>
2023-01-04 13:00:05 +0100

    Pnpm: Fix vue-i18n warning in browser console

--- services/app/frontend/vite.config.ts
+++ services/app/frontend/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
+import { resolve, dirname } from 'node:path'
+import { fileURLToPath } from 'url'
 import vue from '@vitejs/plugin-vue'
 import vuetify from "vite-plugin-vuetify";
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +12,11 @@ export default defineConfig({
     vuetify({
       autoImport: true,
     }),
+      VueI18nPlugin({
+        /* options */
+        // locale messages resource pre-compile option
+        include: resolve(dirname(fileURLToPath(import.meta.url)), './src/i18n/**'),
+      })
   ],
   server: {
     port: 3000
