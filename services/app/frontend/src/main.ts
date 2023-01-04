397aaf74d1c88bfad084acd1c6b123c0b8475d88
Timo Stovermann <tsto@d-velop.de>
2023-01-04 12:18:36 +0100

    Pnpm: Add vuetify

--- services/app/frontend/src/main.ts
+++ services/app/frontend/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import vuetify from "../plugins/vuetify";
 
-createApp(App).mount('#app')
+const app = createApp(App)
+    .use(vuetify);
+
+app.mount('#app')
