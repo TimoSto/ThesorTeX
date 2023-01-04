60b4d1cb3902f8551c2a0c56ffeadfe2f76477a9
Timo Stovermann <tsto@d-velop.de>
2023-01-04 16:02:29 +0100

    Services: Add go filesystems for assets

--- services/app/internal/handlers/root/root.go
+++ services/app/internal/handlers/root/root.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/TimoSto/ThesorTeX/frontend/assets"
+	"github.com/TimoSto/ThesorTeX/services/app/frontend/assets"
 	"github.com/TimoSto/ThesorTeX/services/app/internal/conf"
 )
 
