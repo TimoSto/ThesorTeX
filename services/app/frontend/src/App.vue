71158e70662489590451960192c6841b714478d5
Timo Stovermann <tsto@d-velop.de>
2023-01-04 12:24:14 +0100

    Pnpm: Add sass

--- services/app/frontend/src/App.vue
+++ services/app/frontend/src/App.vue
@@ -3,10 +3,21 @@
 
 <template>
   <v-app>
-    <v-btn>test</v-btn>
+    <div class="outer">
+      <div class="inner">
+        <v-btn>test</v-btn>
+      </div>
+    </div>
   </v-app>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.outer{
+  background-color: red;
+  padding: 10px;
+  & .inner {
+    background-color: green;
+    padding: 100px;
+  }
+}
 </style>
