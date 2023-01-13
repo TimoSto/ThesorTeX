dacbb2f6836c49ae31a2d0b7dac993b89779874f
Timo Stovermann <tsto@d-velop.de>
2023-01-13 17:01:55 +0100

    Repo: Delete current

--- testing/playwright/src/support/OurWorld.ts
+++
@@ -1,6 +0,0 @@
-import { World as CucumberWorld } from "@cucumber/cucumber";
-import { BrowserContext, Page } from "playwright";
-export interface OurWorld extends CucumberWorld {
-    context: BrowserContext;
-    page: Page;
-}
