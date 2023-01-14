bf5e356a494aaa6f88c8e9e9e6d9c1e435a20515
Timo Stovermann <tsto@d-velop.de>
2023-01-14 08:38:01 +0100

    Repo: Add eslint package

--- services/website/frontend/src/App.vue
+++ services/website/frontend/src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <v-app>
     <v-main>
-      <v-btn color="primary" variant="text">Hallo</v-btn>
-      <v-btn color="secondary">Test</v-btn>
+      <v-btn
+        color="primary"
+        variant="text"
+      >
+        Hallo
+      </v-btn>
+      <v-btn
+        color="secondary"
+      >
+        Test
+      </v-btn>
     </v-main>
-
   </v-app>
 </template>
 
