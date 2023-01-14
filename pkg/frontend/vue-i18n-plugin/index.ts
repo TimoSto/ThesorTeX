e82b1cc3794221ce933717c5e0c1856d649e25d2
Timo Stovermann <tsto@d-velop.de>
2023-01-14 15:30:35 +0100

    Repo: Add i18n package

---
+++ pkg/frontend/vue-i18n-plugin/index.ts
@@ -0,0 +1,16 @@
+import {createI18n, I18n} from "vue-i18n";
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+
+export default function CreateI18n(german: any, english: any): I18n<{ de: any, en: any }> {
+    return createI18n({
+        legacy: false, // you must set `false`, to use Composition API
+        locale: window.navigator.language.indexOf('de') >= 0 ? 'de' : 'en',
+        fallbackLocale: 'en', // set fallback locale
+        messages: {
+            de: german,
+            en: english
+        },
+    })
+}
+
+export { VueI18nPlugin }
