d2a1eaefb15a34dee554b317035014764fd5f880
Timo Stovermann <tsto@d-velop.de>
2023-01-15 16:42:03 +0100

    App: Domain: GetAllProjects

--- services/app/internal/domain/projects/get_all_projects.go
+++ services/app/internal/domain/projects/get_all_projects.go
@@ -1,6 +1,8 @@
 package projects
 
 import (
+	"encoding/json"
+
 	"github.com/TimoSto/ThesorTeX/pkg/backend/pathbuilder"
 	"github.com/TimoSto/ThesorTeX/services/app/internal/config"
 	"github.com/TimoSto/ThesorTeX/services/app/internal/filesystem"
@@ -16,7 +18,18 @@ func GetAllProjects(fs filesystem.FileSystem, cfg config.Config) ([]ProjectMetaD
 
 	for _, d := range dirs {
 		path := pathbuilder.GetPathInProject(cfg.ProjectsDir, d, metaDataFile)
-		fs.ReadFile(path)
+		file, err := fs.ReadFile(path)
+		if err != nil {
+			return nil, err
+		}
+
+		var data ProjectMetaData
+		err = json.Unmarshal(file, &data)
+		if err != nil {
+			return nil, err
+		}
+
+		projects = append(projects, data)
 	}
 
 	return projects, nil
---
+++ services/app/internal/domain/projects/get_all_projects_test.go
@@ -0,0 +1,46 @@
+package projects
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/TimoSto/ThesorTeX/pkg/backend/pathbuilder"
+	"github.com/TimoSto/ThesorTeX/services/app/internal/config"
+	"github.com/TimoSto/ThesorTeX/services/app/internal/filesystem/fake"
+)
+
+func TestGetAllProjects(t *testing.T) {
+	fs := fake.FileSystem{}
+
+	cfg := config.Config{
+		ProjectsDir: "projects/",
+	}
+
+	existingData := ProjectMetaData{
+		Name:            "test1",
+		Created:         time.Date(2000, 1, 1, 8, 0, 0, 0, time.Local).Format("2006-01-02 15:04"),
+		LastEdited:      time.Date(2001, 1, 1, 8, 0, 0, 0, time.Local).Format("2006-01-02 15:04"),
+		NumberOfEntries: 2,
+	}
+
+	data, _ := json.Marshal(existingData)
+	path := pathbuilder.GetPathInProject(cfg.ProjectsDir, "test", metaDataFile)
+
+	fs.WriteFile(path, data)
+	fs.CreateDirectory("test")
+
+	projects, err := GetAllProjects(&fs, cfg)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	if len(projects) != 1 {
+		t.Errorf("expected to find 1 project, got %v", len(projects))
+	}
+
+	if !reflect.DeepEqual(projects[0], existingData) {
+		t.Errorf("expected%v, got %v", existingData, projects[0])
+	}
+}
