92627f3ed85bd3b5ccd445f64699e1922ff1f7c4
Timo Stovermann <tsto@d-velop.de>
2023-01-16 19:13:55 +0100

    Vitest-vuetify: Add vue-i18n

--- pkg/frontend/vitest-vuetify/index.ts
+++ pkg/frontend/vitest-vuetify/index.ts
@@ -2,10 +2,11 @@ import * as components from "vuetify/components"
 import * as directives from "vuetify/directives"
 import {createVuetify} from "vuetify";
 import {MountingOptions} from "@vue/test-utils";
+import {createI18n} from "vue-i18n";
 
 const vuetify = createVuetify({ components, directives });
 
-export default function CreateVuetifyMountingOptions(options: MountingOptions<any>): MountingOptions<any> {
+export default function CreateVuetifyMountingOptions(options: MountingOptions<any>, locale?: any): MountingOptions<any> {
     if( !options.global ) {
         options.global = {}
     }
@@ -15,6 +16,17 @@ export default function CreateVuetifyMountingOptions(options: MountingOptions<an
     }
 
     options.global.plugins.push(vuetify);
+
+    if( locale ) {
+        const i18n = createI18n({
+            locale: "en",
+            messages: {
+                en: locale
+            },
+            legacy: false
+        })
+        options.global.plugins.push(i18n);
+    }
 
     return options;
 }
