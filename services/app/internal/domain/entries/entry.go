e4020fb2ea75303834827b818a37c36efcc8edc9
Timo Stovermann <tsto@d-velop.de>
2023-01-19 15:41:13 +0100

    App: Get all entries for project

---
+++ services/app/internal/domain/entries/entry.go
@@ -0,0 +1,11 @@
+package entries
+
+type Entry struct {
+	Key      string
+	Category string
+	Fields   []string
+}
+
+const (
+	entriesFile = "/data/bib_entries.json"
+)
---
+++ services/app/internal/domain/entries/get_all_entries.go
@@ -0,0 +1,24 @@
+package entries
+
+import (
+	"encoding/json"
+
+	"github.com/TimoSto/ThesorTeX/pkg/backend/pathbuilder"
+	"github.com/TimoSto/ThesorTeX/services/app/internal/config"
+	"github.com/TimoSto/ThesorTeX/services/app/internal/filesystem"
+)
+
+func GetAllEntries(project string, fs filesystem.FileSystem, cfg config.Config) ([]Entry, error) {
+	file, err := fs.ReadFile(pathbuilder.GetPathInProject(cfg.ProjectsDir, project, entriesFile))
+	if err != nil {
+		return nil, err
+	}
+
+	var entries []Entry
+	err = json.Unmarshal(file, &entries)
+	if err != nil {
+		return nil, err
+	}
+
+	return entries, nil
+}
