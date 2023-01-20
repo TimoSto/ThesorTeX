c226ce0a58f59a0c1360a493cf0b080599dbf168
Timo Stovermann <tsto@d-velop.de>
2023-01-20 17:17:28 +0100

    CategoryEditor: Empty cards

--- services/app/frontend/src/pages/CategoryEditor.vue
+++ services/app/frontend/src/pages/CategoryEditor.vue
@@ -3,6 +3,51 @@
     <template #bar>
       <v-toolbar-title>{{ categoryName }}</v-toolbar-title>
     </template>
+    <template #content>
+      <div class="fullsize-card-container">
+        <v-card elevation="3">
+          <v-expansion-panels :model-value="0">
+            <v-expansion-panel>
+              <v-expansion-panel-title>
+                Allgemeines
+              </v-expansion-panel-title>
+              <v-expansion-panel-text>
+              </v-expansion-panel-text>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-card>
+      </div>
+      <div class="fullsize-card-container">
+        <v-card elevation="3">
+          <v-card elevation="3">
+            <v-expansion-panels :model-value="0">
+              <v-expansion-panel>
+                <v-expansion-panel-title>
+                  Literatureintrag
+                </v-expansion-panel-title>
+                <v-expansion-panel-text>
+                </v-expansion-panel-text>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </v-card>
+        </v-card>
+      </div>
+      <div class="fullsize-card-container">
+        <v-card elevation="3">
+          <v-card elevation="3">
+            <v-expansion-panels :model-value="0">
+              <v-expansion-panel>
+                <v-expansion-panel-title>
+                  Zitate
+                </v-expansion-panel-title>
+                <v-expansion-panel-text>
+                </v-expansion-panel-text>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </v-card>
+        </v-card>
+      </div>
+    </template>
   </ToolbarAndContent>
 </template>
 
