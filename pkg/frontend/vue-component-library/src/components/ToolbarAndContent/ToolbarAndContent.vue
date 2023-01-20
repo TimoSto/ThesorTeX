775d4400f21d12e2f7ed9f541383103a4fa22923
Timo Stovermann <tsto@d-velop.de>
2023-01-20 17:30:43 +0100

    ToolbarAndContent: ElevateOnScroll

--- pkg/frontend/vue-component-library/src/components/ToolbarAndContent/ToolbarAndContent.vue
+++ pkg/frontend/vue-component-library/src/components/ToolbarAndContent/ToolbarAndContent.vue
@@ -1,22 +1,25 @@
 <template>
-  <v-toolbar
-    v-if="!props.hideBar"
-    :color="props.barColor"
-    density="compact"
-    elevation="1"
-    style="z-index: 1000"
-
-  >
-    <slot name="bar" />
-  </v-toolbar>
-  <v-sheet
-    class="content"
-  >
-    <slot name="content" />
-  </v-sheet>
+  <div ref="root">
+    <v-toolbar
+      v-if="!props.hideBar"
+      :color="props.barColor"
+      density="compact"
+      :elevation="elevation"
+      style="z-index: 1000"
+    >
+      <slot name="bar" />
+    </v-toolbar>
+    <v-sheet
+      class="content"
+    >
+      <slot name="content" />
+    </v-sheet>
+  </div>
 </template>
 
 <script setup lang="ts">
+import {onMounted, ref} from "vue";
+
 const props = defineProps({
   hideBar: {
     type: Boolean,
@@ -26,7 +29,20 @@ const props = defineProps({
     type: String,
     default: "background"
   }
-})
+});
+
+// data
+const elevation = ref(0);
+const root = ref<HTMLElement | null>(null);
+onMounted(() => {
+  root.value!.querySelector(".content")!.addEventListener("scroll", (e) => {
+    if ((e.target! as HTMLElement).scrollTop > 0) {
+      elevation.value = 1;
+    } else {
+      elevation.value = 0;
+    }
+  })
+});
 </script>
 
 <style scoped>
