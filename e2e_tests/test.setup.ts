623c5b0482535c500c615648ae01913899676aa1
Timo Stovermann <tsto@d-velop.de>
2023-01-29 11:15:31 +0100

    E2E: headless

--- e2e_tests/test.setup.ts
+++ e2e_tests/test.setup.ts
@@ -10,7 +10,7 @@ BeforeAll(async function () {
     // Browsers are expensive in Playwright so only create 1
     browser = await chromium.launch({
         // Not headless so we can watch test runs
-        headless: false,
+        headless: true,
         // Slow so we can see things happening
         slowMo: 50,
     });
