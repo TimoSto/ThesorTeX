141455bd42153744e6ca9175ffd5cc9a9bf75cb4
Timo Stovermann <tsto@d-velop.de>
2023-01-29 13:09:32 +0100

    App: Fix cite fields being added to bibFields when entry editor was opened

--- services/app/frontend/src/domain/entry/JoinFields.ts
+++ services/app/frontend/src/domain/entry/JoinFields.ts
@@ -1,6 +1,8 @@
 import {Field} from "../category/Category";
 
 export default function JoinFields(bib: Field[], cite: Field[]): Field[] {
+    bib = JSON.parse(JSON.stringify(bib));
+    cite = JSON.parse(JSON.stringify(cite));
     const bibNames = bib.map(f => f.Name);
     cite.forEach(f => {
         if (bibNames.indexOf(f.Name) === -1) {
