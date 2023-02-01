6bd5f9cbb0012853976f8b4096b7a5fa4a6ad6a8
Timo Stovermann <tsto@d-velop.de>
2023-02-01 14:07:23 +0100

    App: dont fail all projects read if one project fails

--- services/app/internal/domain/projects/get_all_projects.go
+++ services/app/internal/domain/projects/get_all_projects.go
@@ -3,6 +3,7 @@ package projects
 import (
 	"encoding/json"
 
+	"github.com/TimoSto/ThesorTeX/pkg/backend/log"
 	"github.com/TimoSto/ThesorTeX/pkg/backend/pathbuilder"
 	"github.com/TimoSto/ThesorTeX/services/app/internal/config"
 	"github.com/TimoSto/ThesorTeX/services/app/internal/filesystem"
@@ -20,13 +21,15 @@ func GetAllProjects(fs filesystem.FileSystem, cfg config.Config) ([]ProjectMetaD
 		path := pathbuilder.GetPathInProject(cfg.ProjectsDir, d, metaDataFile)
 		file, err := fs.ReadFile(path)
 		if err != nil {
-			return nil, err
+			log.Error("could not read metaData file: %v", err)
+			continue
 		}
 
 		var data ProjectMetaData
 		err = json.Unmarshal(file, &data)
 		if err != nil {
-			return nil, err
+			log.Error("could not deserialize metaData file: %v", err)
+			continue
 		}
 
 		projects = append(projects, data)
