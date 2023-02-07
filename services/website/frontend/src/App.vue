3ea000086f79cf953727179c3f4073d30af46505
Timo Stovermann <tsto@d-velop.de>
2023-02-07 14:23:59 +0100

    Website: AppBar and scroll

--- services/website/frontend/src/App.vue
+++ services/website/frontend/src/App.vue
@@ -1,27 +1,40 @@
 <template>
   <v-app>
+    <v-app-bar color="primary" :elevation="elevation" style="z-index: 1000">
+      <v-toolbar-title>ThesorTeX</v-toolbar-title>
+    </v-app-bar>
     <v-main>
-      <v-btn
-        color="primary"
-        variant="text"
-      >
-        Hallo
-      </v-btn>
-      <v-btn
-        color="secondary"
-      >
-        Test
-      </v-btn>
+      <v-sheet class="content">
+        <div style="height: 1234px"></div>
+      </v-sheet>
     </v-main>
   </v-app>
 </template>
 
-<script>
-export default {
-  name: "App"
-}
+<script lang="ts" setup>
+
+// data
+import {onMounted, ref} from "vue";
+
+const elevation = ref(0);
+const root = ref<HTMLElement | null>(null);
+onMounted(() => {
+  root.value!.querySelector(".content")!.addEventListener("scroll", (e) => {
+    if ((e.target! as HTMLElement).scrollTop > 0) {
+      elevation.value = 1;
+    } else {
+      elevation.value = 0;
+    }
+  });
+});
 </script>
 
 <style scoped>
-
+.content {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  overflow-y: auto;
+  height: calc(100vh - 64px)
+}
 </style>
