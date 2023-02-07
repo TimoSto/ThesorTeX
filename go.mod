873adc789253202f74d9ea2b8dd8b10b8c54a769
Timo Stovermann <tsto@d-velop.de>
2023-02-07 13:20:43 +0100

    Website: lambda is callable via aws cli

--- go.mod
+++ go.mod
@@ -3,8 +3,13 @@ module github.com/TimoSto/ThesorTeX
 go 1.18
 
 require (
+	github.com/aws/aws-lambda-go v1.37.0
+	github.com/aws/aws-sdk-go v1.44.195
 	github.com/justinas/alice v1.2.0
 	gopkg.in/ini.v1 v1.67.0
 )
 
-require github.com/stretchr/testify v1.8.1 // indirect
+require (
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+)
---
+++ terraform/lambda.tf
@@ -0,0 +1,51 @@
+locals {
+  lambda_file = "../artifacts/website/lambda.zip"
+}
+
+resource "aws_lambda_function" "lambda_func" {
+  filename         = local.lambda_file
+  function_name    = "thesortex-website"
+  handler          = "cmd"
+  source_code_hash = base64sha256(local.lambda_file)
+  runtime          = "go1.x"
+  role             = aws_iam_role.lambda_exec.arn
+}
+
+# Assume role setup
+resource "aws_iam_role" "lambda_exec" {
+  name_prefix = "thesortex-website"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
+
+# Attach role to Managed Policy
+variable "iam_policy_arn" {
+  description = "IAM Policy to be attached to role"
+  type        = list(string)
+
+  default = [
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  ]
+}
+
+resource "aws_iam_policy_attachment" "role_attach" {
+  name       = "policy-thesortex-website"
+  roles      = [aws_iam_role.lambda_exec.id]
+  count      = length(var.iam_policy_arn)
+  policy_arn = element(var.iam_policy_arn, count.index)
+}
