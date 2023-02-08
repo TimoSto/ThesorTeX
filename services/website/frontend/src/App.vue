a10b3ea5602cd5763a3e5755eb95dbe3d39b77ba
Timo Stovermann <tsto@d-velop.de>
2023-02-08 13:23:37 +0100

    Website: SVG as bg img

--- services/website/frontend/src/App.vue
+++ services/website/frontend/src/App.vue
@@ -1,26 +1,24 @@
 <template>
   <v-app>
-    <v-app-bar color="primary" :elevation="elevation" style="z-index: 1000">
-      <v-toolbar-title>ThesorTeX</v-toolbar-title>
-    </v-app-bar>
     <v-main>
-      <ToolbarAndContent :hide-bar="true" @scroll="elevation=1" @no-scroll="elevation=0">
-        <template #content>
-          <div style="width: 200px; height: 1234px; background-color: #0d47a1"></div>
-        </template>
-      </ToolbarAndContent>
+      <div class="partial">
+        <div style="height: 500px">
+          hallo
+        </div>
+      </div>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-import {ref} from "vue";
 
-// data
-const elevation = ref(0);
 
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.partial {
+  width: 100%;
+  height: 500px;
+  background-image: url('data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 166 166"><polygon fill="red" points="83 26.8 65.7 61.8 27.1 67.4 55 94.7 48.5 133.2 83 115 117.5 133.2 111 94.7 138.9 67.4 100.3 61.8 83 26.8 83 26.8"/></svg>');
+}
 </style>
