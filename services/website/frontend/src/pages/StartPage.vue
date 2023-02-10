9ce45cf2235d6b6ee80634619f72be51ad34eea3
Timo Stovermann <tsto@d-velop.de>
2023-02-10 14:06:33 +0100

    Website: cv icon

--- services/website/frontend/src/pages/StartPage.vue
+++ services/website/frontend/src/pages/StartPage.vue
@@ -14,7 +14,7 @@
     </v-container>
   </div>
   <div class="bg-container">
-    <v-container class="bg-transparent mb-6">
+    <v-container class="bg-transparent pb-6">
       <v-row>
         <v-col cols="6">
           <h2 class="text-h3 font-weight-bold pt-6 pb-6">Vorlage für wissenschaftliche Arbeiten</h2>
@@ -27,23 +27,23 @@
           </v-btn>
         </v-col>
         <v-col cols="6">
-          icon
+          <TemplateIcon :hide-icon="true" style="display: block; margin: 0 auto; max-height: 350px" />
         </v-col>
       </v-row>
     </v-container>
   </div>
   <div class="bg-container" style="background-color: #f5f5f5">
-    <v-container class="bg-transparent mb-6">
+    <v-container class="bg-transparent pb-6">
       <v-row>
         <v-col cols="6">
-          icon
+          <TemplateIcon :hide-icon="false" style="display: block; margin: 0 auto; max-height: 350px" />
         </v-col>
         <v-col cols="6">
-          <h2 class="text-h3 font-weight-bold">Literaturmanagement</h2>
-          <p class="text-h6">Mit diesem Tool kannst du dein Literaturverzeichnis bequem und individuell
+          <h2 class="text-h3 font-weight-bold pt-6 pb-6">Literaturmanagement</h2>
+          <p class="text-h6 pb-6">Mit diesem Tool kannst du dein Literaturverzeichnis bequem und individuell
             zusammenbauen. Formatiere deine Literatur nach deinen Wünschen, lege Einträge händisch an oder
             importiere deine bibliothek aus <i>Citavi</i>.</p>
-          <v-btn>
+          <v-btn color="primary">
             Mehr erfahren
           </v-btn>
         </v-col>
@@ -51,22 +51,22 @@
     </v-container>
   </div>
   <div class="bg-container">
-    <v-container class="bg-transparent mb-6">
+    <v-container class="bg-transparent pb-6">
       <v-row>
         <v-col cols="6">
-          icon
-        </v-col>
-        <v-col cols="6">
-          <h2 class="text-h3 font-weight-bold">Vorlage für einen Lebenslauf</h2>
-          <p class="text-h6">
+          <h2 class="text-h3 font-weight-bold pt-6 pb-6">Vorlage für einen Lebenslauf</h2>
+          <p class="text-h6 pb-6">
             Wenn du mithilfe der hier angebotenen Unterstützungen deinen Abschluss geschafft hast, kannst du
             dich
             mit <a
             href="/sdfsdf">diese
             Vorlage</a> für deien Lebenslauf auf deinen nächsten Job bewerben.</p>
-          <v-btn>
+          <v-btn color="primary">
             Dokumentation der Vorlage
           </v-btn>
+        </v-col>
+        <v-col cols="6">
+          <CVIcon style="display: block; margin: 0 auto; max-height: 350px" />
         </v-col>
       </v-row>
     </v-container>
@@ -74,7 +74,8 @@
 </template>
 
 <script lang="ts" setup>
-</script>
+import TemplateIcon from "../components/TemplateIcon.vue";
+import CVIcon from "../components/CVIcon.vue";</script>
 
 <style scoped>
 
