9b45937c87c919b183aee28f35546e09b59b2184
Timo Stovermann <tsto@d-velop.de>
2023-02-11 12:04:26 +0100

    Website: add overview btn

--- services/website/frontend/src/App.vue
+++ services/website/frontend/src/App.vue
@@ -6,7 +6,10 @@
           <v-row align="center" class="fill-height">
             <v-toolbar-title class="text-h4 font-weight-bold">ThesorTeX</v-toolbar-title>
             <v-spacer />
-            <v-btn color="primary" to="/downloads">
+            <v-btn color="primary" to="/" v-if="currentPage !== 'Home'">
+              Übersicht
+            </v-btn>
+            <v-btn color="primary" to="/downloads" v-if="currentPage !== 'Downloads'">
               Downloads
             </v-btn>
             <v-btn color="primary" to="/tutorials">
@@ -28,7 +31,10 @@
 </template>
 
 <script lang="ts" setup>
-import {ref} from "vue";
+import {computed, ref} from "vue";
+import {useRouter} from "vue-router";
+
+const router = useRouter();
 
 // data
 const elevation = ref(0);
@@ -42,6 +48,10 @@ window.addEventListener("resize", () => {
   } else {
     smallDisplay.value = false;
   }
+});
+
+const currentPage = computed(() => {
+  return router.currentRoute.value.name;
 });
 
 </script>
