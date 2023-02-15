16d30a733dceb1ebf71c8907ccd8407a0824a17f
Timo Stovermann <tsto@d-velop.de>
2023-02-15 19:35:23 +0100

    SVG-Template: thesis hat good

--- services/website/frontend/src/components/svgs/TestSVG.ts
+++ services/website/frontend/src/components/svgs/TestSVG.ts
@@ -227,11 +227,138 @@ const p1: SVGPartial = {
     ]
 };
 
+const hatPart1: PathPart[] = [
+    {
+        vector: {
+            x: 50,
+            y: -20
+        }
+    },
+    {
+        vector: {
+            x: 150,
+            y: -70
+        }
+    },
+    {
+        vector: {
+            x: 250,
+            y: -20
+        }
+    },
+    {
+        vector: {
+            x: 150,
+            y: 30
+        }
+    },
+    {
+        vector: {
+            x: 50,
+            y: -20
+        }
+    }
+];
+
+const hatPart2: PathPart[] = [
+    {
+        vector: {
+            x: 90,
+            y: 5
+        }
+    },
+    {
+        vector: {
+            x: 90,
+            y: 55
+        }
+    },
+    {
+        arc: {
+            radius: 50,
+            radiusY: 20,
+            clockwise: true,
+            rotation: 0,
+            end: {
+                x: 210,
+                y: 55
+            }
+        }
+    },
+    {
+        vector: {
+            x: 210,
+            y: 5
+        }
+    },
+];
+
+const hatPart3: PathPart[] = [
+    {
+        vector: {
+            x: 230,
+            y: -10
+        }
+    },
+    {
+        vector: {
+            x: 230,
+            y: 15
+        }
+    },
+    {
+        arc: {
+            radius: 5,
+            clockwise: true,
+            rotation: 0,
+            end: {
+                x: 230,
+                y: 25
+            }
+        }
+    },
+    {
+        arc: {
+            radius: 5,
+            clockwise: true,
+            rotation: 0,
+            end: {
+                x: 230,
+                y: 15
+            }
+        }
+    },
+    {
+        vector: {
+            x: 230,
+            y: 40
+        }
+    },
+];
+
+const hat: SVGPartial = {
+    strokeColor: "rgba(var(--v-theme-on-background), 1)",
+    strokeWidth: "10",
+    fillColor: "white",
+    scale: 1,
+    angle: 0,
+    translate: {
+        x: 0,
+        y: 0,
+    },
+    parts: [
+        hatPart2,
+        hatPart1,
+        hatPart3
+    ]
+};
+
 export const TestSVG: TemplateSVG = {
     viewBox: "-250 -250 500 500",
     width: 500,
     height: 500,
     partials: [
         p1,
+        hat
     ]
 };
