0ec88a77098c72224668db008e7c90937eaf1824
Timo Stovermann <tsto@d-velop.de>
2023-02-16 16:27:39 +0100

    SVG-Template: laptop mark

--- services/website/frontend/src/components/svgs/LaptopSVG.ts
+++ services/website/frontend/src/components/svgs/LaptopSVG.ts
@@ -9,13 +9,13 @@ const display: PathPart[] = [
     {
         vector: {
             x: -210,
-            y: -200
+            y: -150
         }
     },
     {
         vector: {
             x: 210,
-            y: -200
+            y: -150
         }
     },
     {
@@ -25,26 +25,26 @@ const display: PathPart[] = [
             clockwise: false,
             end: {
                 x: 230,
-                y: -180
+                y: -130
             }
         }
     },
     {
         vector: {
             x: 230,
-            y: 50
+            y: 100
         }
     },
     {
         vector: {
             x: -230,
-            y: 50
+            y: 100
         }
     },
     {
         vector: {
             x: -230,
-            y: -180
+            y: -130
         }
     },
     {
@@ -54,7 +54,7 @@ const display: PathPart[] = [
             clockwise: false,
             end: {
                 x: -210,
-                y: -200
+                y: -150
             }
         }
     },
@@ -64,13 +64,13 @@ const keyboard: PathPart[] = [
     {
         vector: {
             x: -240,
-            y: 70
+            y: 120
         }
     },
     {
         vector: {
             x: 240,
-            y: 70
+            y: 120
         }
     },
     {
@@ -80,14 +80,14 @@ const keyboard: PathPart[] = [
             clockwise: false,
             end: {
                 x: 250,
-                y: 80
+                y: 130
             }
         }
     },
     {
         vector: {
             x: -250,
-            y: 80
+            y: 130
         }
     },
     {
@@ -97,7 +97,7 @@ const keyboard: PathPart[] = [
             clockwise: false,
             end: {
                 x: -240,
-                y: 70
+                y: 120
             }
         }
     },
@@ -113,18 +113,68 @@ const laptop: SVGPartial = {
     ]
 };
 
+const laptopMark: SVGPartial = {
+    strokeColor: "white",
+    strokeWidth: "5",
+    fillColor: "white",
+    parts: [
+        [
+            {
+                vector: {
+                    x: -50,
+                    y: 122
+                }
+            },
+            {
+                vector: {
+                    x: 50,
+                    y: 122
+                }
+            },
+            {
+                arc: {
+                    radius: 3,
+                    rotation: 0,
+                    clockwise: false,
+                    end: {
+                        x: 50,
+                        y: 128
+                    }
+                }
+            },
+            {
+                vector: {
+                    x: -50,
+                    y: 128
+                }
+            },
+            {
+                arc: {
+                    radius: 3,
+                    rotation: 0,
+                    clockwise: false,
+                    end: {
+                        x: -50,
+                        y: 122
+                    }
+                }
+            },
+        ]
+    ]
+};
+
 const thesisPart1 = JSON.parse(JSON.stringify(ThesisSVG.partials[0]));
 thesisPart1.scale = 0.45;
 thesisPart1.translate = {
-    x: 40,
-    y: -160
+    x: 20,
+    y: -50
 };
 
 const thesisPart2 = JSON.parse(JSON.stringify(ThesisSVG.partials[1]));
 thesisPart2.scale = 0.45;
 thesisPart2.translate = {
     x: 0,
-    y: -160
+    y: -50
 };
 
 export const LaptopSVG: TemplateSVG = {
@@ -133,6 +183,7 @@ export const LaptopSVG: TemplateSVG = {
     height: 500,
     partials: [
         laptop,
+        laptopMark,
         thesisPart1,
         thesisPart2
     ]
