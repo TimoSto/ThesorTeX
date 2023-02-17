d4383aaae5654f6dba4429e28e1b31366fe14068
Timo Stovermann <tsto@d-velop.de>
2023-02-17 08:35:03 +0100

    SVG-Template: person in cv look ok

--- services/website/frontend/src/components/svgs/CVSVG.ts
+++ services/website/frontend/src/components/svgs/CVSVG.ts
@@ -85,13 +85,13 @@ const line1: PathPart[] = [
     {
         vector: {
             x: -100,
-            y: -50
+            y: -30
         },
     },
     {
         vector: {
             x: 100,
-            y: -50
+            y: -30
         },
     }
 ];
@@ -100,13 +100,13 @@ const line2: PathPart[] = [
     {
         vector: {
             x: -100,
-            y: 0
+            y: 20
         },
     },
     {
         vector: {
             x: 100,
-            y: 0
+            y: 20
         },
     }
 ];
@@ -115,13 +115,13 @@ const line3: PathPart[] = [
     {
         vector: {
             x: -100,
-            y: 50
+            y: 70
         },
     },
     {
         vector: {
             x: 100,
-            y: 50
+            y: 70
         },
     }
 ];
@@ -139,11 +139,105 @@ const paper: SVGPartial = {
     ]
 };
 
+const body: PathPart[] = [
+    {
+        vector: {
+            x: -50,
+            y: -70
+        },
+    },
+    {
+        vector: {
+            x: 50,
+            y: -70
+        },
+    },
+    {
+        arc: {
+            radius: 30,
+            radiusY: 50,
+            rotation: 0,
+            clockwise: true,
+            end: {
+                x: 20,
+                y: -120
+            }
+        }
+    },
+    {
+        arc: {
+            radius: 20,
+            radiusY: 10,
+            rotation: 0,
+            clockwise: false,
+            end: {
+                x: -20,
+                y: -120
+            }
+        }
+    },
+    {
+        arc: {
+            radius: 30,
+            radiusY: 50,
+            rotation: 0,
+            clockwise: true,
+            end: {
+                x: -50,
+                y: -70
+            }
+        }
+    },
+];
+
+const head: PathPart[] = [
+    {
+        vector: {
+            x: 0,
+            y: -130
+        }
+    },
+    {
+        arc: {
+            radius: 25,
+            rotation: 0,
+            clockwise: true,
+            end: {
+                x: 0,
+                y: -180
+            }
+        }
+    },
+    {
+        arc: {
+            radius: 25,
+            rotation: 0,
+            clockwise: true,
+            end: {
+                x: 0,
+                y: -130
+            }
+        }
+    }
+];
+
+const person: SVGPartial = {
+    strokeColor: "rgba(var(--v-theme-on-background), 1)",
+    strokeWidth: "10",
+    fillColor: "rgba(var(--v-theme-on-background), 1)",
+    scale: 1,
+    parts: [
+        body,
+        head
+    ]
+};
+
 export const CVSVG: TemplateSVG = {
     width: 500,
     height: 500,
     viewBox: "-250 -250 500 500",
     partials: [
-        paper
+        paper,
+        person
     ]
 };
