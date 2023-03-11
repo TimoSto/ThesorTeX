7f960f29cd3add6f10d9240b443cf3ec3e1810c8
Timo Stovermann <tsto@d-velop.de>
2023-03-11 13:12:56 +0100

    Website: Remove unused function

--- services/website/cmd/main.go
+++ services/website/cmd/main.go
@@ -13,16 +13,6 @@ import (
 	"github.com/TimoSto/ThesorTeX/services/website/internal/handlers"
 )
 
-// Handler is your Lambda function handler
-// It uses Amazon API Gateway request/responses provided by the aws-lambda-go/events package,
-// However you could use other event sources (S3, Kinesis etc), or JSON-decoded primitive types such as 'string'.
-func Handler(w http.ResponseWriter, r *http.Request) {
-	// stdout and stderr are sent to AWS CloudWatch Logs
-	log.Info("got Request %s", r.URL.Path)
-
-	w.Write([]byte(fmt.Sprintf("hallo %s", r.URL.Path)))
-}
-
 func main() {
 	log.Info("Start lambda")
 
