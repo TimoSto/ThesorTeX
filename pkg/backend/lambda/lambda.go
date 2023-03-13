c5515cd98915d3bc239cb10e1c3fc74594453165
Timo Stovermann <tsto@d-velop.de>
2023-03-13 17:02:23 +0100

    Lambda: Split in 2 files

--- pkg/backend/lambda/lambda.go
+++ pkg/backend/lambda/lambda.go
@@ -18,16 +18,6 @@ import (
 
 //TODO: write differently
 
-// implements https://godoc.org/net/http#ResponseWriter
-type responseWriter struct {
-	statusCode int
-	header     http.Header
-	body       *bytes.Buffer
-
-	wroteHeader bool
-	snapHeader  http.Header // snapshot of HeaderMap at first Write
-}
-
 func Start(handler http.Handler) {
 	lambda.Start(ApplyHandlerToLambda(handler))
 }
@@ -122,88 +112,4 @@ func mapHeader(e *events.APIGatewayProxyRequest) *http.Header {
 		result.Add(k, v)
 	}
 	return result
-}
-
-func (rw *responseWriter) response() (*events.APIGatewayProxyResponse, error) {
-	response := &events.APIGatewayProxyResponse{}
-
-	if rw.snapHeader != nil && len(rw.snapHeader) > 0 {
-		response.MultiValueHeaders = rw.snapHeader
-	}
-
-	ct := rw.header.Get("Content-Type")
-	// to fix font types
-	if strings.HasPrefix(ct, "font/") {
-		response.IsBase64Encoded = true
-	}
-
-	if rw.body.Len() > 0 {
-		b, err := ioutil.ReadAll(rw.body)
-		if err != nil {
-			return nil, err
-		}
-		if response.IsBase64Encoded {
-			enc := base64.StdEncoding.EncodeToString(b)
-			response.Body = enc
-		} else {
-			response.Body = string(b)
-		}
-	}
-
-	if rw.statusCode == 0 {
-		rw.statusCode = http.StatusOK
-	}
-
-	response.StatusCode = rw.statusCode
-
-	return response, nil
-}
-
-// Header necessary to implement the responsewriter interface
-func (rw *responseWriter) Header() http.Header {
-	return rw.header
-}
-
-// Write necessary to implement the responsewriter interface
-func (rw *responseWriter) Write(buf []byte) (int, error) {
-	rw.writeHeader(buf)
-	if rw.body != nil {
-		rw.body.Write(buf)
-	}
-	return len(buf), nil
-}
-
-// writes header if it hasn't been called already and trys to detect the content-type if it is not set explicitly
-func (rw *responseWriter) writeHeader(b []byte) {
-	if rw.wroteHeader {
-		return
-	}
-
-	m := rw.Header()
-	_, hasType := m["Content-Type"]
-	hasTE := m.Get("Transfer-Encoding") != ""
-	if !hasType && !hasTE {
-		m.Set("Content-Type", http.DetectContentType(b))
-	}
-
-	rw.WriteHeader(http.StatusOK)
-}
-
-func (rw *responseWriter) WriteHeader(statusCode int) {
-	if rw.wroteHeader {
-		return
-	}
-	rw.statusCode = statusCode
-	rw.wroteHeader = true
-	rw.snapHeader = cloneHeader(rw.header)
-}
-
-func cloneHeader(h http.Header) http.Header {
-	h2 := make(http.Header, len(h))
-	for k, v := range h {
-		v2 := make([]string, len(v))
-		copy(v2, v)
-		h2[k] = v2
-	}
-	return h2
 }
