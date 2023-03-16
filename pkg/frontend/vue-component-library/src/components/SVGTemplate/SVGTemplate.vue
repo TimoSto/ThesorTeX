06936b853738e0d82e84dea5f4f9b87867e68847
Timo Stovermann <tsto@d-velop.de>
2023-03-16 17:39:02 +0100

    SVGTemplate: Fix path error occurring on multiple usages

--- pkg/frontend/vue-component-library/src/components/SVGTemplate/SVGTemplate.vue
+++ pkg/frontend/vue-component-library/src/components/SVGTemplate/SVGTemplate.vue
@@ -1,8 +1,8 @@
 <template>
-  <svg :width="svg.width" :height="svg.height" :viewBox="svg.viewBox" xmlns="http://www.w3.org/2000/svg">
+  <svg :width="svgEl.width" :height="svgEl.height" :viewBox="svgEl.viewBox" xmlns="http://www.w3.org/2000/svg">
     <!--    <path stroke="black" stroke-width="5" d="M0,-250 l0,500 z" />-->
     <!--    <path stroke="black" stroke-width="5" d="M-250,0 l500,0 z" />-->
-    <g v-for="(part, i) in svg.partials" :key="`partial-${i}`"
+    <g v-for="(part, i) in svgEl.partials" :key="`partial-${i}`"
        :transform="generateTransform(part)">
       <path v-for="(p, j) in part.parts" :key="`partial-${i}-${j}`" :stroke="part.strokeColor"
             :stroke-width="part.strokeWidth" :fill="part.fillColor"
@@ -13,11 +13,15 @@
 
 <script lang="ts" setup>
 import {TemplateSVG} from "./helper/SVG";
-import {PropType} from "vue";
+import {computed, PropType} from "vue";
 import {generatePath, generateTransform} from "./helper/generatePath";
 
 const props = defineProps({
   svg: Object as PropType<TemplateSVG>,
+});
+
+const svgEl = computed(() => {
+  return JSON.parse(JSON.stringify(props.svg));
 });
 
 </script>
