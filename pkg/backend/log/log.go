fdc14daaedaaaf383af65f02cee8bec76d760f20
Timo Stovermann <tsto@d-velop.de>
2023-03-25 16:06:47 +0100

    Log: Debug prefix

--- pkg/backend/log/log.go
+++ pkg/backend/log/log.go
@@ -52,5 +52,5 @@ func Fatal(msg string, a ...any) {
 }
 
 func Debug(msg string, a ...any) {
-	fmt.Println(fmt.Sprintf("INFO [%v] %v", time.Now().Format("2006-01-02 15:04"), fmt.Sprintf(msg, a...)))
+	fmt.Println(fmt.Sprintf("DEBUG [%v] %v", time.Now().Format("2006-01-02 15:04"), fmt.Sprintf(msg, a...)))
 }
