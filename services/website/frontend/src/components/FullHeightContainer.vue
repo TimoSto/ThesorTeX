f7912fa27e6589a0695dd6aa6d4c846bd9b6f62e
Timo Stovermann <tsto@d-velop.de>
2023-03-25 17:13:00 +0100

    Website: FullHeightContainer - make scroll buttons absolute and global

--- services/website/frontend/src/components/FullHeightContainer.vue
+++ services/website/frontend/src/components/FullHeightContainer.vue
@@ -1,21 +1,9 @@
 <template>
   <div style="min-height: 100vh; display: flex; align-items: center; box-sizing: border-box; position: relative;"
        :style="bgColor" ref="container">
-    <v-container :style="padding">
+    <v-container>
       <slot />
     </v-container>
-    <v-btn icon size="65" color="transparent" flat class="scroll-btn scroll-up" @click="$emit('prev')" v-if="!first">
-      <v-icon size="55"
-              :style="`${bg === 'gradient' ? 'color: rgba(255, 255, 255, 0.75);' : 'color: rgba(0, 0, 0, 0.5);'}`">
-        mdi-arrow-up-circle-outline
-      </v-icon>
-    </v-btn>
-    <v-btn icon size="65" color="transparent" flat class="scroll-btn scroll-down" @click="$emit('next')" v-if="!last">
-      <v-icon size="55"
-              :style="`${bg === 'gradient' ? 'color: rgba(255, 255, 255, 0.75);' : 'color: rgba(0, 0, 0, 0.5);'}`">
-        mdi-arrow-down-circle-outline
-      </v-icon>
-    </v-btn>
   </div>
 </template>
 
@@ -23,7 +11,7 @@
 export default {
   name: "FullHeightContainer",
   props: ["bg", "first", "last", "top"],
-  emits: ["next", "prev"],
+  emits: ["scrolledIntoView"],
   computed: {
     bgColor() {
       switch (this.bg) {
@@ -35,34 +23,29 @@ export default {
           return "background-color: white; "
       }
     },
-    padding() {
-      return this.first ? "" : "";
-    },
   },
-  watch: {
-    top: function () {
-      if (this.top) {
-        this.$refs.container.scrollIntoView({behavior: "smooth"});
-      }
+  methods: {
+    scrollToTop() {
+      this.$refs.container.scrollIntoView({behavior: "smooth"});
     }
+  },
+  mounted() {
+    window.addEventListener("scroll", () => {
+      let docViewTop = document.documentElement.scrollTop;
+      let docViewBottom = docViewTop + window.innerHeight;
+
+      let elemTop = this.$refs.container.offsetTop;
+      let elemBottom = elemTop + this.$refs.container.offsetHeight;
+
+      //TODO: only show after a specific diff from top ( a little more scroll, maybe 50% or 25%)
+      if ((elemTop < docViewBottom) && (elemTop > docViewTop)) {
+        this.$emit("scrolledIntoView");
+      }
+    })
   }
 }
 </script>
 
 <style scoped lang="scss">
-.scroll-btn {
-  position: absolute;
 
-  &.scroll-down {
-    right: 0;
-    bottom: 0;
-    transform: translate(-20px, -20px);
-  }
-
-  &.scroll-up {
-    right: 0;
-    top: 0;
-    transform: translate(-20px, 20px);
-  }
-}
 </style>
--- services/website/frontend/src/components/FullHeightLayout.vue
+++ services/website/frontend/src/components/FullHeightLayout.vue
@@ -1,38 +1,73 @@
 <template>
   <FullHeightContainer v-for="i in pages" :bg="i === 1 ? 'gradient' : i % 2 !== 0 && !white ? 'gray' : ''"
-                       :top="i === top"
-                       :first="i === 1"
-                       :last="i === pages"
-                       @next="top = i + 1" @prev="top = i - 1">
+                       @scrolled-into-view="setLowestVisible(i)" ref="container">
     <slot :name="`content-${i}`" :jumpTo="jumpTo" />
   </FullHeightContainer>
+  <v-btn icon size="65" color="transparent" flat class="scroll-btn scroll-up" v-if="lowestVisible > 1"
+         @click="jumpTo(lowestVisible - 1)">
+    <v-icon size="55"
+            :style="`${lowestVisible === 1 ? 'color: rgba(255, 255, 255, 0.75);' : 'color: rgba(0, 0, 0, 0.5);'}`">
+      mdi-arrow-up-circle-outline
+    </v-icon>
+  </v-btn>
+  <v-btn icon size="65" color="transparent" flat class="scroll-btn scroll-down" v-if="lowestVisible < pages"
+         @click="jumpTo(lowestVisible + 1)">
+    <v-icon size="55"
+            :style="`${lowestVisible === 1 ? 'color: rgba(255, 255, 255, 0.75);' : 'color: rgba(0, 0, 0, 0.5);'}`">
+      mdi-arrow-down-circle-outline
+    </v-icon>
+  </v-btn>
 </template>
 
-<script>
+<script lang="ts">
+import {defineComponent} from "vue";
 import FullHeightContainer from "./FullHeightContainer.vue";
 
-export default {
+export default defineComponent({
   name: "FullHeightLayout",
   components: {FullHeightContainer},
   props: ["pages", "white"],
   data: () => {
     return {
-      top: 1
-    }
+      top: 1,
+      lowestVisible: 1
+    };
   },
   mounted() {
     window.addEventListener("scroll", () => {
-      this.top = -1;
+      if (window.scrollY === 0) {
+        this.setLowestVisible(1);
+      }
     });
   },
   methods: {
-    jumpTo(i) {
-      this.top = i;
+    jumpTo(i: number) {
+      console.log(i, this.lowestVisible);
+      (this.$refs.container as any[])[i - 1].scrollToTop();
+    },
+    setLowestVisible(n: number) {
+      if (n !== this.lowestVisible) {
+        this.lowestVisible = n;
+      }
     }
   }
-}
+});
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.scroll-btn {
+  position: fixed;
 
+  &.scroll-down {
+    right: 0;
+    bottom: 0;
+    transform: translate(-20px, -20px);
+  }
+
+  &.scroll-up {
+    right: 0;
+    top: 0;
+    transform: translate(-20px, 20px);
+  }
+}
 </style>
