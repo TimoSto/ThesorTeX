b9147106a3181f0e34518d7773974cfcca080e72
Timo Stovermann <tsto@d-velop.de>
2023-04-01 17:57:31 +0200

    RevealJS: text smaller

--- pkg/frontend/vue-component-library/src/components/RevealJS/RevealJS.vue
+++ pkg/frontend/vue-component-library/src/components/RevealJS/RevealJS.vue
@@ -7,19 +7,19 @@
           <section v-for="p in d.Pages" class="scrollable">
             {{ p.Title }}
             <template v-for="g in p.Groups">
-              <p v-if="g.Type === 'TEXT'">
+              <p v-if="g.Type === 'TEXT'" class="small">
                 <span v-for="e in g.Elements">
                   {{ e.Content }}
                 </span>
               </p>
-              <pre v-if="g.Type === 'CODE'">
+              <pre v-if="g.Type === 'CODE'" class="small">
                 <code data-trim data-noescape>
                   <template v-for="e in g.Elements">
                     {{ e.Content }}
                   </template>
                 </code>
               </pre>
-              <ul v-if="g.Type === 'LIST'">
+              <ul v-if="g.Type === 'LIST'" class="small">
                 <li v-for="e in g.Elements">
                   {{ e.Content }}
                 </li>
@@ -82,5 +82,9 @@ onMounted(() => {
   overflow-y: auto !important;
   overflow-x: hidden !important;
   height: 700px;
+}
+
+.small {
+  font-size: 25px;
 }
 </style>
