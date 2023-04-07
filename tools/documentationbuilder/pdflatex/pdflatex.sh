0b11918cf818b2cc8c219cc42b473dfc833250cc
Timo Stovermann <tsto@d-velop.de>
2023-04-07 13:47:40 +0200

    DocumentationBuilder: build template using bazel

--- tools/documentationbuilder/pdflatex/pdflatex.sh
+++ tools/documentationbuilder/pdflatex/pdflatex.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
+echo "pwd"
+echo $PWD
+
+cd $2
+
+echo "pwd"
+echo $PWD
+
 set -eu
 
 echo "first run..."
 
+echo "srcs"
 echo "$1"
 
 pdflatex "$1"
@@ -11,5 +20,9 @@ pdflatex "$1"
 echo "second run..."
 
 pdflatex "$1"
+
+ls
+
+# cp main.pdf "$3"
 
 echo "finished"
