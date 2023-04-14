cb7f306b439225b24ee3727668942e8f8523e4ff
Timo Stovermann <tsto@d-velop.de>
2023-04-14 15:51:30 +0200

    Website: add version in config package

--- scripts/workspace_status.sh
+++ scripts/workspace_status.sh
@@ -3,3 +3,6 @@ set -euo pipefail
 
 # Get versions of applications
 echo APP_VERSION "$(./scripts/env.sh THESIS_TOOL VERSIONS)"
+
+# Get versions of applications
+echo WEBSITE_VERSION "$(./scripts/env.sh WEBSITE VERSIONS)-$(git rev-parse HEAD)"
