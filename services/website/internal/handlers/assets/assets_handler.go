ee91440e2c5afa6ef6a08d205f25f81b6d8c3599
Timo Stovermann <tsto@d-velop.de>
2023-04-22 13:10:49 +0200

    Website: add caching for assets

--- services/website/internal/handlers/assets/assets_handler.go
+++ services/website/internal/handlers/assets/assets_handler.go
@@ -3,12 +3,15 @@ package assets
 import (
 	"net/http"
 
+	"github.com/TimoSto/ThesorTeX/pkg/backend/http/caching"
 	"github.com/TimoSto/ThesorTeX/services/website/frontend/assets"
 )
 
 func HandleAssets() func(w http.ResponseWriter, r *http.Request) {
 	httpFs := http.FileServer(http.FS(assets.Assets))
 	fn := func(w http.ResponseWriter, r *http.Request) {
+		caching.ApplyCacheTimes(r, &w)
+
 		httpFs.ServeHTTP(w, r)
 	}
 
