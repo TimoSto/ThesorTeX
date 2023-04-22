bd67f2db9abd04323183d4c15b5d9f9adc137fee
Timo Stovermann <tsto@d-velop.de>
2023-04-22 11:17:20 +0200

    ImageViewer: Introduce component

---
+++ pkg/frontend/vue-component-library/src/components/ImageViewer/ImageViewer.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-img :src="image" @click="opened=true" style="cursor: pointer" />
+  <v-dialog v-model="opened" fullscreen>
+    <v-img :src="image" style="max-height: calc(100% - 50px); margin-top: 25px;" />
+    <v-btn icon position="fixed" size="large" style="right: 25px; top: 25px;" @click="opened=false">
+      <v-icon size="large">mdi-close</v-icon>
+    </v-btn>
+  </v-dialog>
+</template>
+
+<script lang="ts" setup>
+import {ref} from "vue";
+
+defineProps({
+  image: {
+    type: String,
+    required: true
+  }
+});
+
+const opened = ref(false);
+</script>
+
+<style scoped>
+
+</style>
--- pkg/frontend/vue-component-library/src/components/index.ts
+++ pkg/frontend/vue-component-library/src/components/index.ts
@@ -3,11 +3,13 @@ import ErrorSuccessDisplay from "./ErrorSuccessDisplay/ErrorSuccessDisplay.vue";
 import SVGTemplate from "./SVGTemplate/SVGTemplate.vue";
 import MarkdownToVuetify from "./MarkdownToVuetify/MarkdownToVuetify.vue";
 import RevealJS from "./RevealJS/RevealJS.vue";
+import ImageViewer from "./ImageViewer/ImageViewer.vue";
 
 export {
     ToolbarAndContent,
     ErrorSuccessDisplay,
     SVGTemplate,
     MarkdownToVuetify,
-    RevealJS
+    RevealJS,
+    ImageViewer,
 };
