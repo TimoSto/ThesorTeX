55598223d283531f221bd3c8136cb8c267edcf27
Timo Stovermann <tsto@d-velop.de>
2023-05-14 14:38:59 +0200

    Website: jumping on new overview

--- services/website/frontend/src/pages/StartPageNew.vue
+++ services/website/frontend/src/pages/StartPageNew.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import {i18nKeys} from "../i18n/keys";
 import {useI18n} from "vue-i18n";
-import {computed} from "vue";
+import {computed, ref} from "vue";
 import {GetThesisSVG} from "../components/svgs/ThesisSVG";
 import {GetLaptopSVG} from "../components/svgs/LaptopSVG";
 import {GetCvSVG} from "../components/svgs/CVSVG";
@@ -9,12 +9,20 @@ import {GetBugSVG} from "../components/svgs/BugSVG";
 import GermanTemplateImg from "../assets/thesis_template_de.png";
 import ThesisToolImg from "../assets/thesis_tool_de.png";
 import CvTemplateImg from "../assets/cv_template_de.png";
+import {WaveContainer} from "@thesortex/vue-component-library/src/components";
 
 const {t} = useI18n();
 
 defineProps({
   smallDisplay: Boolean
 });
+
+const container0 = ref<InstanceType<typeof WaveContainer> | null>(null);
+const container1 = ref<InstanceType<typeof WaveContainer> | null>(null);
+const container2 = ref<InstanceType<typeof WaveContainer> | null>(null);
+const container3 = ref<InstanceType<typeof WaveContainer> | null>(null);
+const container4 = ref<InstanceType<typeof WaveContainer> | null>(null);
+const container5 = ref<InstanceType<typeof WaveContainer> | null>(null);
 
 // computed
 
@@ -42,29 +50,53 @@ const bugSVG = computed(() => {
 
   return svg;
 });
+
+// methods
+function jumpTo(n: number) {
+  console.log(container0.value, (container1.value! as HTMLElement));
+  switch (n) {
+    case 0:
+      (container0.value!.$el as HTMLElement).scrollIntoView({behavior: "smooth"});
+      break;
+    case 1:
+      (container1.value!.$el as HTMLElement).scrollIntoView({behavior: "smooth"});
+      break;
+    case 2:
+      (container2.value!.$el as HTMLElement).scrollIntoView({behavior: "smooth"});
+      break;
+    case 3:
+      (container3.value!.$el as HTMLElement).scrollIntoView({behavior: "smooth"});
+      break;
+    case 4:
+      (container4.value!.$el as HTMLElement).scrollIntoView({behavior: "smooth"});
+      break;
+  }
+}
 </script>
 
 <template>
-  <WaveContainer :wave-func="1" :small-display="smallDisplay" bg-color="#001220" wave-color="#008833">
+  <WaveContainer :wave-func="1" :small-display="smallDisplay" ref="container0" bg-color="#001220" wave-color="#008833">
     <v-row>
       <v-col :cols="smallDisplay ? 12: 6">
-        <h2 class="text-h3 font-weight-bold text-white pa-4 pt-15">{{ t(i18nKeys.StartPage.Title) }}</h2>
+        <h2 class="text-h3 font-weight-bold text-white pa-4 pt-15" ref="container0">{{
+            t(i18nKeys.StartPage.Title)
+          }}</h2>
         <p class="text-h5 text-white pa-4 pb-5">{{ t(i18nKeys.StartPage.Subtitle) }}</p>
         <v-list class="text-h6"
                 style="background-color: transparent; color: white; font-weight: bold; cursor: pointer;">
-          <v-list-item v-ripple>
+          <v-list-item v-ripple @click="jumpTo(1)">
             <template #prepend>
               <v-icon style="opacity: 1">mdi-arrow-right-circle-outline</v-icon>
             </template>
             {{ t(i18nKeys.StartPage.ThesisTemplateTitle) }}
           </v-list-item>
-          <v-list-item v-ripple>
+          <v-list-item v-ripple @click="jumpTo(2)">
             <template #prepend>
               <v-icon style="opacity: 1">mdi-arrow-right-circle-outline</v-icon>
             </template>
             {{ t(i18nKeys.StartPage.ThesisToolTitle) }}
           </v-list-item>
-          <v-list-item v-ripple>
+          <v-list-item v-ripple @click="jumpTo(3)">
             <template #prepend>
               <v-icon style="opacity: 1">mdi-arrow-right-circle-outline</v-icon>
             </template>
@@ -79,7 +111,7 @@ const bugSVG = computed(() => {
       </v-col>
     </v-row>
   </WaveContainer>
-  <WaveContainer :wave-func="2" :small-display="smallDisplay" bg-color="#008833" wave-color="#001220">
+  <WaveContainer :wave-func="2" :small-display="smallDisplay" ref="container1" bg-color="#008833" wave-color="#001220">
     <v-row>
       <v-col v-if="!smallDisplay" cols="6" class="d-flex" style="justify-content: center; align-items: center;">
         <ImageViewer :image="GermanTemplateImg" />
@@ -119,7 +151,7 @@ const bugSVG = computed(() => {
       </v-col>
     </v-row>
   </WaveContainer>
-  <WaveContainer :wave-func="3" :small-display="smallDisplay" bg-color="#001220" wave-color="#008833">
+  <WaveContainer :wave-func="3" :small-display="smallDisplay" ref="container2" bg-color="#001220" wave-color="#008833">
     <v-row>
       <v-col :cols="smallDisplay ? 12 : 6">
         <h2 class="text-h3 text-white font-weight-bold pt-6 pb-6">{{ t(i18nKeys.StartPage.ThesisToolTitle) }}</h2>
@@ -148,7 +180,7 @@ const bugSVG = computed(() => {
       </v-col>
     </v-row>
   </WaveContainer>
-  <WaveContainer :wave-func="2" :small-display="smallDisplay" bg-color="#008833" wave-color="#001220">
+  <WaveContainer :wave-func="2" :small-display="smallDisplay" ref="container3" bg-color="#008833" wave-color="#001220">
     <v-row>
       <v-col v-if="!smallDisplay" cols="6" class="d-flex" style="justify-content: center; align-items: center;">
         <ImageViewer :image="CvTemplateImg" />
@@ -176,7 +208,7 @@ const bugSVG = computed(() => {
       </v-col>
     </v-row>
   </WaveContainer>
-  <WaveContainer :wave-func="-1" :small-display="smallDisplay" bg-color="#001220" wave-color="#008833">
+  <WaveContainer :wave-func="-1" :small-display="smallDisplay" ref="container4" bg-color="#001220" wave-color="#008833">
     <v-row>
       <v-col :cols="smallDisplay ? 12 : 6">
         <h2 class="text-h3 text-white font-weight-bold pt-6 pb-6">{{ t(i18nKeys.StartPage.KnownIssuesTitle) }}</h2>
