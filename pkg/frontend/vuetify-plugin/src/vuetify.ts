217ab6cd06cf75ed6a2fb30b001d51279831b4a1
Timo Stovermann <tsto@d-velop.de>
2023-05-18 17:10:47 +0200

    website: use dark theme thesisTool: use light theme

--- pkg/frontend/vuetify-plugin/src/vuetify.ts
+++ pkg/frontend/vuetify-plugin/src/vuetify.ts
@@ -12,31 +12,35 @@ import "vuetify/styles";
 import {createVuetify} from "vuetify";
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-export const vuetifyInstance = createVuetify({
-    theme: {
-        themes: {
-            light: {
-                colors: {
-                    primary: "#008833",
-                    primaryText: "#FFFFFF",
-                    secondary: "#52634f",
-                    accent: "#000000",
-                    error: "#ba1a1a",
-                    background: "#001220",
-                    primaryDark: "#15a835",
+export function GetVuetifyInstance(dark: boolean) {
+    console.log(dark);
+    return createVuetify({
+        theme: {
+            defaultTheme: dark ? "dark" : "light",
+            themes: {
+                light: {
+                    colors: {
+                        primary: "#008833",
+                        primaryText: "#FFFFFF",
+                        secondary: "#52634f",
+                        accent: "#000000",
+                        error: "#ba1a1a",
+                        background: "#FFFFFF",
+                        primaryDark: "#15a835",
+                    }
+                },
+                dark: {
+                    dark: true,
+                    colors: {
+                        primary: "#15a835",
+                        secondary: "#b9ccb4",
+                        accent: "#FFFFFF",
+                        error: "#CF6679",
+                        background: "#121212",
+                        primaryDark: "#15a835",
+                    }
                 }
             },
-            dark: {
-                dark: true,
-                colors: {
-                    primary: "#15a835",
-                    secondary: "#b9ccb4",
-                    accent: "#FFFFFF",
-                    error: "#CF6679",
-                    background: "#121212",
-                    primaryDark: "#15a835",
-                }
-            }
         },
-    },
-});
+    });
+}
--- services/website/frontend/src/main.ts
+++ services/website/frontend/src/main.ts
@@ -1,6 +1,6 @@
 import {createApp} from "vue";
 import App from "./App.vue";
-import {vuetifyInstance} from "@thesortex/vuetify-plugin";
+import {GetVuetifyInstance} from "@thesortex/vuetify-plugin";
 import {installLib} from "@thesortex/vue-component-library";
 import router from "./router";
 import CreateI18n from "@thesortex/vue-i18n-plugin";
@@ -8,7 +8,7 @@ import {german} from "./i18n/german";
 import {english} from "./i18n/english";
 
 const app = createApp(App)
-    .use(vuetifyInstance)
+    .use(GetVuetifyInstance(true))
     .use(installLib)
     .use(CreateI18n(german, english))
     .use(router);
