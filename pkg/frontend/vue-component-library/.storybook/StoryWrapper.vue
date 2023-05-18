8569435244ae1c75007f3b6501c3ade8609208b2
Timo Stovermann <tsto@d-velop.de>
2023-05-18 13:45:06 +0200

    storybook: works with vuetify with theme in vue-component-lib (basic)

---
+++ pkg/frontend/vue-component-library/.storybook/StoryWrapper.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app :theme="themeName">
+    <v-main>
+      <slot name="story"></slot>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+defineProps({
+  themeName: String
+})
+</script>
--- pkg/frontend/vue-component-library/.storybook/preview.ts
+++ pkg/frontend/vue-component-library/.storybook/preview.ts
@@ -1,7 +1,10 @@
 import {setup} from "@storybook/vue3";
 import {vuetifyInstance} from "@thesortex/vuetify-plugin";
+import {withVuetifyTheme} from "./withVuetifyTheme.decorator";
 
 setup((app) => {
     // Registers your app's plugins into Storybook
     app.use(vuetifyInstance);
 });
+
+export const decorators = [withVuetifyTheme];
