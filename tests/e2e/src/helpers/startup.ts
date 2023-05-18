959e7f3aaf304d40b5223a31386338baaff297b3
Timo Stovermann <tsto@d-velop.de>
2023-05-18 17:34:36 +0200

    e2e: generalize sut setup

---
+++ tests/e2e/src/helpers/startup.ts
@@ -0,0 +1,57 @@
+import {APIRequestContext, chromium, ChromiumBrowser} from "playwright";
+import {spawn} from "child_process";
+import {expect, request} from "@playwright/test";
+
+export async function StartupBrowser(): Promise<ChromiumBrowser> {
+    const browser = await chromium.launch({
+        // Not headless so we can watch test runs
+        headless: true,
+        // Slow so we can see things happening
+        slowMo: 50,
+    });
+
+    return browser;
+}
+
+export async function StartupSutIfExists(): Promise<any> {
+    let sut: any;
+
+    const sutExec = process.env.EXECUTABLE;
+    if (sutExec) {
+        console.log("start sut executable: ", sutExec);
+        sut = spawn(sutExec, {
+            // stdio: "ignore",
+            detached: false,
+        });
+        sut.on("exit", (code: number) => {
+            if (code && code !== 0) {
+                throw `System under test exited with code ${code}`;
+            }
+        });
+        sut.on("error", (err: any) => {
+            throw "Could not start system under test executable";
+        });
+        // sut.stdout.on("data", (data: any) => console.log(data.toString()));
+        sut.stderr.on("data", (data: any) => console.error(data.toString()));
+
+        console.log("sbu", process.env.SYSTEM_BASE_URL);
+        const ctx = await request.newContext({
+            baseURL: process.env.SYSTEM_BASE_URL,
+            ignoreHTTPSErrors: true,
+        });
+        await expect.poll(() => isReachable(ctx), {timeout: 10000}).toBeTruthy();
+    } else {
+        console.log("not sut executable found in env variables");
+    }
+
+    return sut;
+}
+
+async function isReachable(ctx: APIRequestContext) {
+    try {
+        await ctx.head("/");
+        return true;
+    } catch {
+        return false;
+    }
+}
--- tests/e2e/src/test.setup.ts
+++ tests/e2e/src/test.setup.ts
@@ -1,56 +1,17 @@
 import {After, AfterAll, Before, BeforeAll} from "@cucumber/cucumber";
-import {APIRequestContext, chromium, ChromiumBrowser, devices} from "playwright";
-import {expect, request} from "@playwright/test";
+import {ChromiumBrowser, devices} from "playwright";
 import {OurWorld} from "./types";
-import {spawn} from "child_process";
+import {StartupBrowser, StartupSutIfExists} from "./helpers/startup";
 
 let browser: ChromiumBrowser;
 let sut: any;
 
 BeforeAll(async function () {
     // Browsers are expensive in Playwright so only create 1
-    browser = await chromium.launch({
-        // Not headless so we can watch test runs
-        headless: true,
-        // Slow so we can see things happening
-        slowMo: 50,
-    });
+    browser = await StartupBrowser();
 
-    const sutExec = process.env.EXECUTABLE;
-    console.log("exec", sutExec);
-    if (sutExec) {
-        sut = spawn(sutExec, {
-            // stdio: "ignore",
-            detached: false,
-        });
-        sut.on("exit", (code: number) => {
-            if (code && code !== 0) {
-                throw `System under test exited with code ${code}`;
-            }
-        });
-        sut.on("error", (err: any) => {
-            throw "Could not start system under test executable";
-        });
-        // sut.stdout.on("data", (data: any) => console.log(data.toString()));
-        sut.stderr.on("data", (data: any) => console.error(data.toString()));
-
-        console.log("sbu", process.env.SYSTEM_BASE_URL);
-        const ctx = await request.newContext({
-            baseURL: process.env.SYSTEM_BASE_URL,
-            ignoreHTTPSErrors: true,
-        });
-        await expect.poll(() => isReachable(ctx), {timeout: 10000}).toBeTruthy();
-    }
+    sut = await StartupSutIfExists();
 });
-
-async function isReachable(ctx: APIRequestContext) {
-    try {
-        await ctx.head("/");
-        return true;
-    } catch {
-        return false;
-    }
-}
 
 AfterAll(async function () {
     await browser.close();
