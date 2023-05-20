6893e6161fe7477678898ed470c9b2ea0992aca3
Timo Stovermann <tsto@d-velop.de>
2023-05-20 12:03:46 +0200

    website: close image viewer on backdrop

--- pkg/frontend/vue-component-library/src/components/ImageViewer/ImageViewer.vue
+++ pkg/frontend/vue-component-library/src/components/ImageViewer/ImageViewer.vue
@@ -1,19 +1,18 @@
 <template>
   <v-img :src="image" @click="opened=true" style="cursor: pointer"
          :style="imageStyles? imageStyles : 'max-height: calc(100vh - 100px)'" />
-  <v-dialog v-model="opened" fullscreen scrim="black" class="darker-scrim">
-    <v-img :src="image"
-           style="max-height: calc(100% - 50px); max-width: calc(100% - 50px); margin-top: 25px; margin-left: 25px;" />
-    <v-btn icon position="fixed" size="large" style="right: 25px; top: 25px;" @click="opened=false">
-      <v-icon size="large">mdi-close</v-icon>
-    </v-btn>
+  <v-dialog v-model="opened" scrim="black" class="darker-scrim" :width="adjustedDimensions.width"
+            :height="adjustedDimensions.height">
+    <v-card theme="light" color="transparent" elevation="0">
+      <img :src="image" />
+    </v-card>
   </v-dialog>
 </template>
 
 <script lang="ts" setup>
-import {ref} from "vue";
+import {computed, onMounted, ref, watch} from "vue";
 
-defineProps({
+const props = defineProps({
   image: {
     type: String,
     required: true
@@ -22,6 +21,48 @@ defineProps({
 });
 
 const opened = ref(false);
+
+const fullWidth = ref(0);
+const fullHeight = ref(0);
+
+const imgWidth = ref(0);
+const imgHeight = ref(0);
+
+// computed
+// TODO: test this
+const adjustedDimensions = computed(() => {
+  const ratio = imgWidth.value / imgHeight.value;
+
+  const maxDimension = ratio > 1 ? fullWidth.value - 50 : fullHeight.value - 50;
+  const scaleFactor = ratio > 1 ? maxDimension / imgWidth.value : maxDimension / imgHeight.value;
+  const secondDimensionScaled = ratio > 1 ? imgHeight.value * scaleFactor : imgWidth.value * scaleFactor;
+
+  return {
+    width: ratio > 1 ? maxDimension : secondDimensionScaled,
+    height: ratio > 1 ? secondDimensionScaled : maxDimension,
+  };
+});
+
+// watchers
+watch(() => props.image, () => {
+  checkImageDimensions();
+});
+
+onMounted(() => {
+  checkImageDimensions();
+  fullWidth.value = window.innerWidth;
+  fullHeight.value = window.innerHeight;
+});
+
+// methods
+function checkImageDimensions() {
+  const img = new Image();
+  img.onload = function () {
+    imgWidth.value = img.width;
+    imgHeight.value = img.height;
+  };
+  img.src = props.image;
+}
 </script>
 
 <style lang="scss">
