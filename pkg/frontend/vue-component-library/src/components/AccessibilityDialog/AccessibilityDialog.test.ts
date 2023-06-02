ec6be8a919c4f2548e7c9f19bd314a43f235f97f
Timo Stovermann <tsto@d-velop.de>
2023-06-02 15:56:10 +0200

    vue-component-lib: wrap tests

--- pkg/frontend/vue-component-library/src/components/AccessibilityDialog/AccessibilityDialog.test.ts
+++ pkg/frontend/vue-component-library/src/components/AccessibilityDialog/AccessibilityDialog.test.ts
@@ -11,69 +11,71 @@ describe("AccessibilityDialog.vue", () => {
 
         fakeDocument = new Document();
     });
-    it("should have closed state", () => {
-        const cmp = mount(AccessibilityDialog, CreateVuetifyMountingOptions({
-            props: {
-                keydownTarget: fakeDocument
-            }
-        }));
+    describe("opened state", () => {
+        it("should have closed state", () => {
+            const cmp = mount(AccessibilityDialog, CreateVuetifyMountingOptions({
+                props: {
+                    keydownTarget: fakeDocument
+                }
+            }));
 
-        expect(cmp.vm.opened).toBe(false);
-        expect(document.body.querySelector(".v-card-title")).toBeFalsy();
-    });
-    it("should open on 2x CTRL", () => {
-        const cmp = mount(AccessibilityDialog, CreateVuetifyMountingOptions({
-            props: {
-                keydownTarget: fakeDocument
-            }
-        }));
-
-        fakeDocument.dispatchEvent(new FakeEvent("keypress", {key: "Ctrl"}));
-        fakeDocument.dispatchEvent(new FakeEvent("keypress", {key: "Ctrl"}));
-        expect(cmp.vm.opened).toBe(true);
-    });
-    it("should not open on 1x CTRL", () => {
-        const cmp = mount(AccessibilityDialog, CreateVuetifyMountingOptions({
-            props: {
-                keydownTarget: fakeDocument
-            }
-        }));
-
-        fakeDocument.dispatchEvent(new FakeEvent("keypress", {key: "Ctrl"}));
-        expect(cmp.vm.opened).toBe(false);
-    });
-    it("should not open on 2x CTRL too far apart", async () => {
-        const cmp = mount(AccessibilityDialog, CreateVuetifyMountingOptions({
-            props: {
-                keydownTarget: fakeDocument
-            }
-        }));
-
-        fakeDocument.dispatchEvent(new FakeEvent("keypress", {key: "Ctrl"}));
-
-        await new Promise(resolve => {
-            setTimeout(resolve, 400);
+            expect(cmp.vm.opened).toBe(false);
+            expect(document.body.querySelector(".v-card-title")).toBeFalsy();
         });
+        it("should open on 2x CTRL", () => {
+            const cmp = mount(AccessibilityDialog, CreateVuetifyMountingOptions({
+                props: {
+                    keydownTarget: fakeDocument
+                }
+            }));
 
-        fakeDocument.dispatchEvent(new FakeEvent("keypress", {key: "Ctrl"}));
+            fakeDocument.dispatchEvent(new FakeEvent("keypress", {key: "Ctrl"}));
+            fakeDocument.dispatchEvent(new FakeEvent("keypress", {key: "Ctrl"}));
+            expect(cmp.vm.opened).toBe(true);
+        });
+        it("should not open on 1x CTRL", () => {
+            const cmp = mount(AccessibilityDialog, CreateVuetifyMountingOptions({
+                props: {
+                    keydownTarget: fakeDocument
+                }
+            }));
 
-        expect(cmp.vm.opened).toBe(false);
-    });
-    it("should open on 2x CTRL 200ms apart", async () => {
-        const cmp = mount(AccessibilityDialog, CreateVuetifyMountingOptions({
-            props: {
-                keydownTarget: fakeDocument
-            }
-        }));
+            fakeDocument.dispatchEvent(new FakeEvent("keypress", {key: "Ctrl"}));
+            expect(cmp.vm.opened).toBe(false);
+        });
+        it("should not open on 2x CTRL too far apart", async () => {
+            const cmp = mount(AccessibilityDialog, CreateVuetifyMountingOptions({
+                props: {
+                    keydownTarget: fakeDocument
+                }
+            }));
 
-        fakeDocument.dispatchEvent(new FakeEvent("keypress", {key: "Ctrl"}));
+            fakeDocument.dispatchEvent(new FakeEvent("keypress", {key: "Ctrl"}));
 
-        await new Promise(resolve =>
-            setTimeout(resolve, 200)
-        );
+            await new Promise(resolve => {
+                setTimeout(resolve, 400);
+            });
 
-        fakeDocument.dispatchEvent(new FakeEvent("keypress", {key: "Ctrl"}));
+            fakeDocument.dispatchEvent(new FakeEvent("keypress", {key: "Ctrl"}));
 
-        expect(cmp.vm.opened).toBe(true);
+            expect(cmp.vm.opened).toBe(false);
+        });
+        it("should open on 2x CTRL 200ms apart", async () => {
+            const cmp = mount(AccessibilityDialog, CreateVuetifyMountingOptions({
+                props: {
+                    keydownTarget: fakeDocument
+                }
+            }));
+
+            fakeDocument.dispatchEvent(new FakeEvent("keypress", {key: "Ctrl"}));
+
+            await new Promise(resolve =>
+                setTimeout(resolve, 200)
+            );
+
+            fakeDocument.dispatchEvent(new FakeEvent("keypress", {key: "Ctrl"}));
+
+            expect(cmp.vm.opened).toBe(true);
+        });
     });
 });
