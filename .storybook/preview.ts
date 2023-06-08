5abc3773fd2a6416662af9963de7818defe4c29a
Timo Stovermann <tsto@d-velop.de>
2023-06-08 14:11:07 +0200

    repo: add mdi font to storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -5,6 +5,8 @@ import {createVuetify} from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
+import "@mdi/font/css/materialdesignicons.css";
+
 const vuetify = createVuetify({
     components,
     directives,
