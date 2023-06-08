2672e0bca64aefa8179f6ca0938ddb2c445c0392
Timo Stovermann <tsto@d-velop.de>
2023-06-08 11:56:08 +0200

    VueCompLib: storybook scroll

--- pkg/frontend/vue-component-library/src/components/ToolbarAndContent/ToolbarAndContent.stories.ts
+++ pkg/frontend/vue-component-library/src/components/ToolbarAndContent/ToolbarAndContent.stories.ts
@@ -1,5 +1,6 @@
 import ToolbarAndContent from "./ToolbarAndContent.vue";
 import {Meta, StoryObj} from "@storybook/vue3";
+import {screen} from "@storybook/testing-library";
 
 const meta: Meta<typeof ToolbarAndContent> = {
     title: "VueComponentLibrary/ToolbarAndContent",
@@ -46,6 +47,42 @@ export const Primary: Story = {
         <v-btn color="white">
             btn
         </v-btn>
-        `
+        `,
+        content: `<p>halllooo</p>`
+    }
+};
+
+export const WithScrollbar: Story = {
+    args: {
+        barColor: "primary",
+        bar: `
+        <v-toolbar-title>Test Title</v-toolbar-title>
+        <v-spacer />
+        <v-btn color="white">
+            btn
+        </v-btn>
+        `,
+        content: `<div style="height: 1500px">hallo</div>`
+    }
+};
+
+export const WithScrollbarScrolled: Story = {
+    args: {
+        barColor: "primary",
+        bar: `
+        <v-toolbar-title>Test Title</v-toolbar-title>
+        <v-spacer />
+        <v-btn color="white">
+            btn
+        </v-btn>
+        `,
+        content: `<div style="height: 1500px">hallo</div>`
+    },
+    play: async () => {
+        const t = screen.getByText("hallo", {
+            selector: "div"
+        });
+        console.log("hi", t);
+        t.parentElement!.scrollTo(0, 300);
     }
 };
