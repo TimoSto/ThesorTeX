09814c4f43e116a4260542e89eb46d99c9fe953e
Timo Stovermann <tsto@d-velop.de>
2023-06-15 17:20:33 +0200

    repo: replace build in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,24 +9,24 @@ echo "cleaning out dir..."
 
 rm -rf "artifacts"
 
-source ./scripts/builder.sh
+source ./scripts/build_lib.sh
 
 if [ "$1" = "thesisTemplate" ] || [ "$1" = "all" ]
 then
   echo "building thesis template"
 
-  build_zip_target //:release_notes_template "artifacts/ReleaseNotes_ThesisTemplate.md"
+  build_target //:release_notes_template "artifacts/ReleaseNotes_ThesisTemplate.md"
 
-  build_zip_target //pkg/tex/project_template:template_zip artifacts/ThesisTemplate.zip
+  build_target //pkg/tex/project_template:template_zip artifacts/ThesisTemplate.zip
 fi
 
 if [ "$1" = "cvTemplate" ] || [ "$1" = "all" ]
 then
   echo "building cv template"
 
-  build_zip_target //:release_notes_cv_template "artifacts/ReleaseNotes_CVTemplate.md"
+  build_target //:release_notes_cv_template "artifacts/ReleaseNotes_CVTemplate.md"
 
-  build_zip_target //pkg/tex/cv_template:template_zip artifacts/CVTemplate.zip
+  build_target //pkg/tex/cv_template:template_zip artifacts/CVTemplate.zip
 fi
 
 if [ "$1" = "thesisTool" ] || [ "$1" = "all" ]
@@ -37,30 +37,30 @@ then
 
   outDir="artifacts/thesisTool"
 
-  build_zip_target //:release_notes_app "artifacts/ReleaseNotes_ThesisTool.md"
+  build_target //:release_notes_app "artifacts/ReleaseNotes_ThesisTool.md"
 
   echo "building for windows..."
 
-  build_windows_target //services/thesisTool/cmd/prod:thesisTool_zip "$outDir/windows/ThesorTeX.zip" "$(pwd)"
+  build_target //services/thesisTool/cmd/prod:thesisTool_zip "$outDir/windows/ThesorTeX.zip" "windows"
 
   echo "building app for linux..."
 
-  build_linux_target //services/thesisTool/cmd/prod:thesisTool_zip "$outDir/linux/ThesorTeX.zip" "$(pwd)"
+  build_target //services/thesisTool/cmd/prod:thesisTool_zip "$outDir/linux/ThesorTeX.zip" "linux"
 
   echo "building app for macOS (AMD)..."
 
-  build_mac_target //services/thesisTool/cmd/prod:thesisTool_zip "$outDir/mac/ThesorTeX.zip" "$(pwd)"
+  build_target //services/thesisTool/cmd/prod:thesisTool_zip "$outDir/mac/ThesorTeX.zip" "mac"
 
   echo "building app for macOS (ARM)..."
 
-  build_mac_m1_target //services/thesisTool/cmd/prod:thesisTool_zip "$outDir/mac_arm/ThesorTeX.zip" "$(pwd)"
+  build_target //services/thesisTool/cmd/prod:thesisTool_zip "$outDir/mac_arm/ThesorTeX.zip" "mac_arm"
 fi
 
 if [ "$1" = "website" ] || [ "$1" = "all" ]
 then
   echo "building website for linux..."
 
-  build_linux_target //services/website/cmd/prod:lambda_zip "artifacts/website/lambda.zip" "$(pwd)"
+  build_target //services/website/cmd/prod:lambda_zip "artifacts/website/lambda.zip" "linux"
 fi
 
 echo "finished"
