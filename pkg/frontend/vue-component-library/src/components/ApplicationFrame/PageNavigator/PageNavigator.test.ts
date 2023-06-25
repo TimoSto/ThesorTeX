26bb2d824c37333237602ecccf745cc47d28f698
Timo Stovermann <tsto@d-velop.de>
2023-06-25 12:04:53 +0200

    vue-component-lib/PageNavigator: add tests

--- pkg/frontend/vue-component-library/src/components/ApplicationFrame/PageNavigator/PageNavigator.test.ts
+++ pkg/frontend/vue-component-library/src/components/ApplicationFrame/PageNavigator/PageNavigator.test.ts
@@ -1,8 +1,49 @@
-import {beforeEach, describe, expect} from "vitest";
+import {beforeEach, describe, expect, it} from "vitest";
 import PageNavigator from "./PageNavigator.vue";
+import {mount} from "@vue/test-utils";
+import {createTestingPinia} from "@pinia/testing";
+import {useApplicationStateStore} from "../../../stores/ApplicationStateStore/ApplicationStateStore";
 
 describe("PageNavigator.vue", () => {
     beforeEach(() => {
         expect(PageNavigator).toBeTruthy();
+    });
+    describe("displaying pages", () => {
+        it("one page", () => {
+            const wrapper = mount(PageNavigator, {
+                global: {
+                    plugins: [createTestingPinia()]
+                }
+            });
+
+            const store = useApplicationStateStore();
+
+            store.history.push("test");
+
+            const pages = wrapper.findAll(".page");
+
+            expect(pages).toHaveLength(1);
+        });
+        it("nav to second page", async () => {
+            const wrapper = mount(PageNavigator, {
+                global: {
+                    plugins: [createTestingPinia({stubActions: false})]
+                }
+            });
+
+            const store = useApplicationStateStore();
+
+            let pages = wrapper.findAll(".page");
+
+            expect(pages).toHaveLength(1);
+
+            store.openPage("test 2");
+
+            await wrapper.vm.$nextTick();
+
+            pages = wrapper.findAll(".page");
+
+            expect(pages).toHaveLength(2);
+        });
     });
 });
