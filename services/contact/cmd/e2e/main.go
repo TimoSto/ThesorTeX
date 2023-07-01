758d54f6d92616b159c5d84c251ddef146a85860
Timo Stovermann <tsto@d-velop.de>
2023-07-01 12:01:56 +0200

    contact: start dynamodb container in e2e

--- services/contact/cmd/e2e/main.go
+++ services/contact/cmd/e2e/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/TimoSto/ThesorTeX/pkg/backend/log"
+import (
+	"github.com/TimoSto/ThesorTeX/pkg/backend/container/dynamodb"
+	"github.com/TimoSto/ThesorTeX/pkg/backend/log"
+	"time"
+)
 
 func main() {
 	log.Info("Starting the fake contact application...")
+
+	c, err := dynamodb.StartDynamoDBLocally()
+	if err != nil {
+		log.Fatal("could not start local dynamo db: %v", err)
+	}
+
+	time.Sleep(5 * time.Second)
+
+	defer c.Purge()
 }
