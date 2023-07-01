0295f648384723b2b8f70d7065be1c24d8dc245d
Timo Stovermann <tsto@d-velop.de>
2023-07-01 11:45:17 +0200

    pkg/backend: add container pkg

---
+++ pkg/backend/container/container.go
@@ -0,0 +1,52 @@
+package container
+
+import (
+	"fmt"
+	"github.com/ory/dockertest/v3"
+	"os"
+	"time"
+)
+
+type Container struct {
+	endpoint string
+	pool     *dockertest.Pool
+	resource *dockertest.Resource
+}
+
+func connectToPool() (*dockertest.Pool, error) {
+	dockerHost := os.Getenv("DOCKER_HOST")
+
+	pool, err := dockertest.NewPool(dockerHost)
+	if err != nil {
+		return nil, fmt.Errorf("could not connect to docker: %s", err)
+	}
+
+	return pool, nil
+}
+
+func RunImage(options *dockertest.RunOptions) (*Container, error) {
+	pool, err := connectToPool()
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := pool.RunWithOptions(options)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := pool.Retry(func() error {
+		time.Sleep(2 * time.Second)
+		return nil
+	}); err != nil {
+		return nil, fmt.Errorf("could not connect to container: %s", err)
+	}
+
+	port := res.GetPort(options.ExposedPorts[0])
+
+	return &Container{
+		endpoint: "http://localhost:" + port,
+		pool:     pool,
+		resource: res,
+	}, nil
+}
