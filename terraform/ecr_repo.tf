be2732885e5aa93017f50fc102bd38f4ca0509bb
Timo Stovermann <tsto@d-velop.de>
2023-07-07 15:17:25 +0200

    contact: add ecr repo in terraform

--- terraform/ecr_repo.tf
+++ terraform/ecr_repo.tf
@@ -6,3 +6,12 @@ resource "aws_ecr_repository" "foo" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_repository" "contact" {
+  name                 = "contact_lambda"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
