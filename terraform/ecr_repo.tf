57f82426c226cbe0de807b0af9393058b0d4b8a6
Timo Stovermann <tsto@d-velop.de>
2023-07-07 16:07:58 +0200

    terraform: moved website ecr into module
    moved {
      from = aws_ecr_repository.website
      to = module.website_lambda_ecr.aws_ecr_repository.ecr_repo
    }

--- terraform/ecr_repo.tf
+++ terraform/ecr_repo.tf
@@ -1,7 +1,4 @@
-moved {
-  from = aws_ecr_repository.website
-  to   = module.website_lambda_ecr.aws_ecr_repository.ecr_repo
-}
+
 resource "aws_ecr_repository" "contact" {
   name                 = "contact_lambda"
   image_tag_mutability = "MUTABLE"
