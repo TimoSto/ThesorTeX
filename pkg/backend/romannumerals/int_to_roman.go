5c53dc66efa69774a526bc4d098bcbc60c9e9a36
Timo Stovermann <tsto@d-velop.de>
2023-07-28 15:13:03 +0200

    pkg/backend/romannumerals: move conversions to global variable

--- pkg/backend/romannumerals/int_to_roman.go
+++ pkg/backend/romannumerals/int_to_roman.go
@@ -5,29 +5,31 @@ import (
 	"strings"
 )
 
+type RomanIntConversion struct {
+	value int
+	digit string
+}
+
+var conversions = []RomanIntConversion{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func IntegerToRoman(number int) string {
 	maxRomanNumber := 3999
 	if number > maxRomanNumber {
 		return strconv.Itoa(number)
-	}
-
-	conversions := []struct {
-		value int
-		digit string
-	}{
-		{1000, "M"},
-		{900, "CM"},
-		{500, "D"},
-		{400, "CD"},
-		{100, "C"},
-		{90, "XC"},
-		{50, "L"},
-		{40, "XL"},
-		{10, "X"},
-		{9, "IX"},
-		{5, "V"},
-		{4, "IV"},
-		{1, "I"},
 	}
 
 	var roman strings.Builder
