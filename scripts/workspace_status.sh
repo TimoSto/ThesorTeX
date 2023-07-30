33bda2ef5e550bc9fe3362147da638d742c8aa3a
Timo Stovermann <tsto@d-velop.de>
2023-07-30 13:59:30 +0200

    repo: change var names in workspace_status.sh

--- scripts/workspace_status.sh
+++ scripts/workspace_status.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Get versions of applications
-echo APP_VERSION "$(./scripts/env.sh THESIS_TOOL VERSIONS)"
+# Get version of thesis tool
+echo THESIS_TOOL_VERSION "$(./scripts/env.sh THESIS_TOOL VERSIONS)"
 
-# Get versions of applications
+# Get version of website
 echo WEBSITE_VERSION "$(./scripts/env.sh WEBSITE VERSIONS)-$(git rev-parse HEAD)"
 
-
+# Get version of contact service
 echo CONTACT_VERSION "$(./scripts/env.sh CONTACT VERSIONS)"
