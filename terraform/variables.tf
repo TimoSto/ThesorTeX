987131773f9d1541d4c5b697528e26e378967e54
Timo Stovermann <tsto@d-velop.de>
2023-07-30 14:51:03 +0200

    website: update image to add3153b

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,7 +12,7 @@ variable "budget_amount" {
 
 variable "website_image_tag" {
   # Update this to the tag of the image in aws ecr
-  default = "f879573b"
+  default = "add3153b"
 }
 
 variable "contact_image_tag" {
