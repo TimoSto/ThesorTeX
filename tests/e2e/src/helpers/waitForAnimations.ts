dc676f30b455fc2ac0b291b18c86da6766f02b83
Timo Stovermann <tsto@d-velop.de>
2023-08-05 14:03:41 +0200

    thesisTool: reenable all tests and fix tests

--- tests/e2e/src/helpers/waitForAnimations.ts
+++ tests/e2e/src/helpers/waitForAnimations.ts
@@ -3,6 +3,10 @@ import {Page} from "playwright";
 export default async function waitForAnimations(page: Page, selectors: string[]) {
     for (const id of selectors) {
         const container2 = await page.$(id);
-        await container2!.waitForElementState("stable");
+        if (container2) {
+            await container2!.waitForElementState("stable");
+        } else {
+            console.warn(`\nWARNING: element ${id} not found\n`);
+        }
     }
 }
