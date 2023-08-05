a962d27db6229d8cc462ef73da6068f8da4eb8a6
Timo Stovermann <tsto@d-velop.de>
2023-08-05 16:20:36 +0200

    website: update to 7d76fef8

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,7 +12,7 @@ variable "budget_amount" {
 
 variable "website_image_tag" {
   # Update this to the tag of the image in aws ecr
-  default = "c59b10f1"
+  default = "7d76fef8"
 }
 
 variable "contact_image_tag" {
