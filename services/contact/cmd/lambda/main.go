d580034d8fe263156c6796a71e6ce8e7d2abf3aa
Timo Stovermann <tsto@d-velop.de>
2023-08-12 19:29:47 +0200

    contact: add backend to prod

--- services/contact/cmd/lambda/main.go
+++ services/contact/cmd/lambda/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/TimoSto/ThesorTeX/pkg/backend/log"
+import (
+	"github.com/TimoSto/ThesorTeX/pkg/backend/log"
+	"github.com/TimoSto/ThesorTeX/services/contact/internal/backend"
+)
 
 func main() {
+	log.Setup(true, "DEBUG")
+
 	log.Info("Starting the aws contact application...")
+
+	cfg := backend.Config{
+		IsProd: true,
+	}
+
+	err := backend.StartApp(cfg)
+	if err != nil {
+		log.Fatal("could not start backend: %v", err)
+	}
 }
