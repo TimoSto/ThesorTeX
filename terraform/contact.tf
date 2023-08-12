339e9723c561522fa4c006b0212c9c8bd340f6af
Timo Stovermann <tsto@d-velop.de>
2023-08-12 15:13:22 +0200

    contact: add lambda deployment

--- terraform/contact.tf
+++ terraform/contact.tf
@@ -4,12 +4,12 @@ module "contact_lambda_ecr" {
   target_name = "contact_lambda"
 }
 
-#module "contact_lambda" {
-#  source          = "./modules/lambda"
-#  image_url       = "${module.contact_lambda_ecr.repository_url}:${var.contact_image_tag}"
-#  function_name   = "contact_lambda"
-#  lambda_policies = [
-#    "arn:aws:s3:::${module.website_s3_artifacts.bucket}",
-#    "arn:aws:s3:::${module.website_s3_artifacts.bucket}/*",
-#  ]
-#}
+module "contact_lambda" {
+  source          = "./modules/lambda"
+  image_url       = "${module.contact_lambda_ecr.repository_url}:${var.contact_image_tag}"
+  function_name   = "contact_lambda"
+  lambda_policies = [
+    "arn:aws:s3:::${module.website_s3_artifacts.bucket}",
+    "arn:aws:s3:::${module.website_s3_artifacts.bucket}/*",
+  ]
+}
