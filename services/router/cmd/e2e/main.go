e60a897f4ce3a0b7efe2a92572ce355087b71b80
Timo Stovermann <tsto@d-velop.de>
2023-08-12 12:53:48 +0200

    router: add e2e executable with website

--- services/router/cmd/e2e/main.go
+++ services/router/cmd/e2e/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"github.com/TimoSto/ThesorTeX/services/router/internal/backend"
+	"log"
+	"os"
+)
+
 func main() {
 
+	sigs := make(chan os.Signal, 1)
+
+	err := backend.StartBackend()
+	if err != nil {
+		log.Fatalf("could not start backend: %v", err)
+	}
+
+	<-sigs
 }
