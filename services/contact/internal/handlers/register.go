6468fa3ad13b50e7a58b12d8a374ce78b72faf45
Timo Stovermann <tsto@d-velop.de>
2023-08-12 13:23:09 +0200

    contact: move prefix to variable

--- services/contact/internal/handlers/register.go
+++ services/contact/internal/handlers/register.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"github.com/TimoSto/ThesorTeX/pkg/backend/versionhandler"
 	"github.com/TimoSto/ThesorTeX/services/contact/internal/config"
 	"github.com/TimoSto/ThesorTeX/services/contact/internal/feedback"
@@ -8,8 +9,10 @@ import (
 	"net/http"
 )
 
-func RegisterHandlers(mux *http.ServeMux, store feedback.Store) {
-	mux.HandleFunc("/contact/version", versionhandler.GetRootHandler(config.Version))
+const prefix = "/contact"
 
-	mux.Handle("/contact/feedback", sendfeedback.GetFeedbackHandler(store))
+func RegisterHandlers(mux *http.ServeMux, store feedback.Store) {
+	mux.HandleFunc(fmt.Sprintf("%s/version", prefix), versionhandler.GetRootHandler(config.Version))
+
+	mux.Handle(fmt.Sprintf("%s/contact/feedback", prefix), sendfeedback.GetFeedbackHandler(store))
 }
