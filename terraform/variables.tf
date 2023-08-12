c12dfae04a2790f6e7975cbb2fc6becc334b2acf
Timo Stovermann <tsto@d-velop.de>
2023-08-12 19:31:49 +0200

    contact: update image to 45d00e89

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,7 +17,7 @@ variable "website_image_tag" {
 
 variable "contact_image_tag" {
   # Update this to the tag of the image in aws ecr
-  default = "c3eb0be5"
+  default = "45d00e89"
 }
 
 variable "router_image_tag" {
