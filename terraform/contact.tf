45d00e896805002c4e330efe315e8efbca6dad37
Timo Stovermann <tsto@d-velop.de>
2023-08-12 15:17:57 +0200

    contact: add api gw deployment (lambda not working yet/does nothing)

--- terraform/contact.tf
+++ terraform/contact.tf
@@ -13,3 +13,10 @@ module "contact_lambda" {
     "arn:aws:s3:::${module.website_s3_artifacts.bucket}/*",
   ]
 }
+
+module "contact_api_gw" {
+  source           = "./modules/apigateway"
+  api_name         = "contact_api"
+  integration_uri  = module.contact_lambda.invoke_arn
+  lambda_func_name = module.contact_lambda.func_name
+}
