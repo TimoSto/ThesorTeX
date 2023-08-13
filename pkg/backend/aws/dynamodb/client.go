4983f351d7be0163e0d71c309138c98514367022
Timo Stovermann <tsto@d-velop.de>
2023-08-13 15:46:50 +0200

    contact: change dynamodb config loading

--- pkg/backend/aws/dynamodb/client.go
+++ pkg/backend/aws/dynamodb/client.go
@@ -2,12 +2,20 @@ package dynamodb
 
 import (
 	"context"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
 func GetDynamoClient(opts func(*config.LoadOptions) error) (*dynamodb.Client, error) {
-	cfg, err := config.LoadDefaultConfig(context.Background(), opts)
+	var cfg aws.Config
+	var err error
+	if opts != nil {
+		cfg, err = config.LoadDefaultConfig(context.Background(), opts)
+	} else {
+		cfg, err = config.LoadDefaultConfig(context.Background())
+	}
+
 	if err != nil {
 		return nil, err
 	}
