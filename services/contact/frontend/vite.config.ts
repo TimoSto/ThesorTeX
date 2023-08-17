2a63abe0e256609a49ca7362a364d3a4b3c8f2a4
Timo Stovermann <tsto@d-velop.de>
2023-08-17 16:37:50 +0200

    contact: build frontend not as lib but with main.ts as entrypoint

--- services/contact/frontend/vite.config.ts
+++ services/contact/frontend/vite.config.ts
@@ -4,7 +4,6 @@ import vue from "@vitejs/plugin-vue";
 // Utilities
 import {defineConfig} from "vite";
 import vuetify from "vite-plugin-vuetify";
-import {resolve} from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,12 +14,22 @@ export default defineConfig({
     }),
   ],
   build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, "src/main.ts"),
-      name: "FeedbackComponent",
-      // the proper extensions will be added
-      fileName: "feedback-component",
-    },
-  },
+    rollupOptions: {
+      input: ["src/main.ts"],
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
+  // build: {
+  //   lib: {
+  //     // Could also be a dictionary or array of multiple entry points
+  //     entry: resolve(__dirname, "src/main.ts"),
+  //     name: "FeedbackComponent",
+  //     // the proper extensions will be added
+  //     fileName: "feedback-component",
+  //   },
+  // },
 });
