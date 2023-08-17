8beb4de923ea1be7ceefaabd112221e4a2c42cc0
Timo Stovermann <tsto@d-velop.de>
2023-08-17 17:15:47 +0200

    contact: cleanup comments in main.ts

--- services/contact/frontend/src/main.ts
+++ services/contact/frontend/src/main.ts
@@ -1,19 +1,11 @@
-/**
- * main.ts
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
-// Components
 import App from "./App.vue";
 import {GetVuetifyInstance} from "@thesortex/vuetify-plugin";
-
-// Composables
 import {createApp} from "vue";
 
 const app = createApp(App)
   .use(GetVuetifyInstance(false));
 
+// function to be used in a dynamic import
 export function renderFeedbackComponent(parent: string) {
   app.mount(parent);
 }
