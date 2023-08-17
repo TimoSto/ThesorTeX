eaaf6380bbba82a9d337f3407082886bfda80125
Timo Stovermann <tsto@d-velop.de>
2023-08-17 15:55:56 +0200

    contact: build frontend lib instead of html

--- services/contact/frontend/vite.config.ts
+++ services/contact/frontend/vite.config.ts
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // Utilities
 import {defineConfig} from "vite";
 import vuetify from "vite-plugin-vuetify";
+import {resolve} from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,4 +14,13 @@ export default defineConfig({
       autoImport: true,
     }),
   ],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/main.ts"),
+      name: "FeedbackComponent",
+      // the proper extensions will be added
+      fileName: "feedback-component",
+    },
+  },
 });
