384112e9ba5693de028e1563c20f9685dfa9a5ba
Timo Stovermann <tsto@d-velop.de>
2023-08-17 17:30:58 +0200

    contact: build frontend via bazel

--- services/contact/frontend/vite.config.ts
+++ services/contact/frontend/vite.config.ts
@@ -5,32 +5,34 @@ import vue from "@vitejs/plugin-vue";
 import {defineConfig} from "vite";
 import vuetify from "vite-plugin-vuetify";
 
-// https://vitejs.dev/config/
+const root = process.env.VITE_ROOT;
+
 export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify({
-      autoImport: true,
-    }),
-  ],
-  base: "/contact/feedbackComponent",
-  build: {
-    rollupOptions: {
-      input: ["src/main.ts"],
-      output: {
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
+    root: root ? root : ".",
+    plugins: [
+        vue(),
+        vuetify({
+            autoImport: true,
+        }),
+    ],
+    base: "/contact/feedbackComponent",
+    build: {
+        rollupOptions: {
+            input: [root ? root + "/src/main.ts" : "src/main.ts"],
+            output: {
+                entryFileNames: `[name].js`,
+                chunkFileNames: `[name].js`,
+                assetFileNames: `assets/[name].[ext]`
+            }
+        }
     }
-  }
-  // build: {
-  //   lib: {
-  //     // Could also be a dictionary or array of multiple entry points
-  //     entry: resolve(__dirname, "src/main.ts"),
-  //     name: "FeedbackComponent",
-  //     // the proper extensions will be added
-  //     fileName: "feedback-component",
-  //   },
-  // },
+    // build: {
+    //   lib: {
+    //     // Could also be a dictionary or array of multiple entry points
+    //     entry: resolve(__dirname, "src/main.ts"),
+    //     name: "FeedbackComponent",
+    //     // the proper extensions will be added
+    //     fileName: "feedback-component",
+    //   },
+    // },
 });
