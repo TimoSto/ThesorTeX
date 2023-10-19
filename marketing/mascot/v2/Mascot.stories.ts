4bb0d019482fb35b469fb8351fab7c7407e0805a
Timo Stovermann <tsto@d-velop.de>
2023-10-19 20:08:01 +0200

    marketing/mascot: add svg as vue component with storybook (head shape)

---
+++ marketing/mascot/v2/Mascot.stories.ts
@@ -0,0 +1,17 @@
+import {Meta, StoryObj} from "@storybook/vue3";
+import Mascot from "./Mascot.vue";
+
+const meta: Meta<typeof Mascot> = {
+    title: "Mascot",
+    component: Mascot,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Mascot>
+
+export const Simple: Story = {
+    args: {
+        // props
+    }
+};
