294ffa296e1703af87da2072c60ab3b1ac4860b0
Timo Stovermann <tsto@d-velop.de>
2023-10-22 12:11:16 +0200

    marketing/mascot: add bad hat

---
+++ marketing/mascot/v2/parts/Hat.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import {computed} from "vue";
+
+const props = defineProps({
+  startX: Number,
+  startY: Number,
+});
+
+const hatPath = computed(() => {
+  let path = `M ${props.startX} ${props.startY} `;
+
+  // lower part
+  path += "q 50 10 95 2";
+  path += "l -1 -20";
+  path += "q -50 -10 -95 -1";
+  path += "l 1 19";
+
+  // top part
+  path += "m 0 -8";
+  path += "l -30 -13";
+  path += "l 90 -35";
+  return path;
+});
+</script>
+
+<template>
+  <path stroke="black" stroke-width="2" fill="gray" :d="hatPath" />
+</template>
+
+<style scoped lang="scss">
+
+</style>
