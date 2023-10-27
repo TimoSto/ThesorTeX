40e7e2d4776ff6d210bae35de20d872c36d42a14
Timo Stovermann <tsto@d-velop.de>
2023-10-27 15:39:01 +0200

    marketing/mascot: change convert script to check for necessary cmd tool

--- marketing/mascot/scripts/svgToPng.sh
+++ marketing/mascot/scripts/svgToPng.sh
@@ -1,4 +1,11 @@
+if ! command -v rsvg-convert &> /dev/null
+then
+    echo "rsvg-convert could not be found"
+    exit 1
+fi
+
 for i in *.svg; do
     [ -f "$i" ] || break
+    echo "converting $i..."
     rsvg-convert "$i" > "${i%.*}.png"
 done
