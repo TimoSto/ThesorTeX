7e9fcbfaec405797c66dd44bc417af1143a9158a
Timo Stovermann <tsto@d-velop.de>
2023-11-01 15:37:58 +0100

    website: update to 94965fd1

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,7 +12,7 @@ variable "budget_amount" {
 
 variable "website_image_tag" {
   # Update this to the tag of the image in aws ecr
-  default = "8b63c043"
+  default = "94965fd1"
 }
 
 variable "contact_image_tag" {
