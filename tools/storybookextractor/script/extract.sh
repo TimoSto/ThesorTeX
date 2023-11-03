b7ced65fb53f7ae02cde0390d2cd04a5d52cfd5b
Timo Stovermann <tsto@d-velop.de>
2023-11-03 15:05:20 +0100

    tools/storybookextractor: infer dist file name from story name

--- tools/storybookextractor/script/extract.sh
+++ tools/storybookextractor/script/extract.sh
@@ -10,11 +10,13 @@ fi
 echo "generating svgs from storybook..."
 
 export STORY_NAME="$1"
-export STORY_DIST="$2"
+export STORY_DIST="$2/${STORY_NAME//-/_}.svg"
 
 pnpm playwright test
 
 echo "generating pngs from svgs..."
+
+cd $2
 
 for i in *.svg; do
     [ -f "$i" ] || break
