8cf530926a2bd4b182a84854fbde9c0290872ae1
Timo Stovermann <tsto@d-velop.de>
2023-11-18 15:04:49 +0100

    marketing/backgrounds: add walls/room (colors bad)

--- marketing/backgrounds/Background.vue
+++ marketing/backgrounds/Background.vue
@@ -1,15 +1,16 @@
 <script setup>
 import Blackboard from "./parts/blackbaord/Blackboard.vue";
-import Chair from "./parts/Chair.vue";
+import Walls from "./parts/Walls.vue";
 </script>
 
 <template>
   <svg xmlns="http://www.w3.org/2000/svg" width="850" height="400"
-       style="shape-rendering: crispEdges; background-color: #ffffff; border: 1px solid black"
+       style="shape-rendering: crispEdges; background-color: #f8f4ea; border: 1px solid black"
        viewBox="0 0 850 400">
-    <Blackboard :start-x="290" :start-y="75" />
+    <Walls :width="850" :height="400" />
+    <Blackboard :start-x="315" :start-y="50" />
     <!--    <Desk :start-x="440" :start-y="250" />-->
-    <Chair :x="100" :y="180" />
+    <!--    <Chair :x="100" :y="180" />-->
   </svg>
 </template>
 
---
+++ marketing/backgrounds/parts/Walls.vue
@@ -0,0 +1,43 @@
+<script setup>
+import {computed} from "vue";
+
+const props = defineProps({
+  width: Number,
+  height: Number,
+})
+
+const floor = computed(() => {
+  let path = `M 0 ${props.height}`;
+
+  path += `l ${props.width / 2 - 220} -${props.height / 2 - 50}`;
+
+  path += "l 440 0";
+
+  path += `l ${props.width / 2 - 220} ${props.height / 2 - 50}`;
+
+  return path;
+})
+
+const wallMiddle = computed(() => {
+  let path = `M ${props.width / 2 - 220} ${props.height / 2 + 50}`;
+
+  path += `l 0 -${props.height / 2 + 50}`;
+
+  path += "l 440 0";
+
+  path += `l 0 ${props.height / 2 + 50} z`;
+
+  return path;
+})
+</script>
+
+<template>
+  <g data-type="walls-n-floor">
+    <path stroke="black" stroke-width="1" :d="floor" fill="#A0A1A6" />
+    <path stroke="black" stroke-width="1" :d="wallMiddle" fill="#f4f3ef" />
+  </g>
+</template>
+
+<style scoped lang="scss">
+
+</style>
