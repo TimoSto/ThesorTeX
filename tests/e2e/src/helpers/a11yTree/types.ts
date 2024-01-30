330546088415b0bc8d0df7e45a6d31791927a81f
Timo Stovermann <tsto@d-velop.de>
2024-01-30 18:13:49 +0100

    tests/e2e: add a11y tree types

--- tests/e2e/src/helpers/a11yTree/types.ts
+++ tests/e2e/src/helpers/a11yTree/types.ts
@@ -1,3 +1,33 @@
+// given by Accessibility api
+export type AxNode = {
+    nodeId: string
+    ignored: boolean
+    ignoredReasons: ValueProperty[]
+    role: TypeValue
+    chromeRole: TypeValue
+    name: TypeValueWithSource
+    properties: ValueProperty[]
+    childIds: string[]
+    parentId: string
+}
+
+export type TypeValue = {
+    type: string
+    value: string
+}
+
+export type TypeValueWithSource = {
+    type: string
+    value: string
+    sources: TypeValue[]
+}
+
+export type ValueProperty = {
+    type: string
+    value: TypeValue
+}
+
+// result
 export type AccessibilityTreeResult = {
     PageTitle?: string
     Nodes: AccessibilityTreeNode[]
