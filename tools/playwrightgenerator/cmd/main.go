3d17cc967624d3ff2fe913ea66e5a392152b5cd3
Timo Stovermann <tsto@d-velop.de>
2024-09-15 15:17:51 +0200

    works

--- tools/playwrightgenerator/cmd/main.go
+++ tools/playwrightgenerator/cmd/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/TimoSto/ThesorTeX/pkg/backend/log"
+	"os"
 	"os/exec"
+	"path"
 )
 
 func main() {
@@ -11,10 +14,25 @@ func main() {
 		log.Fatal("could not find npx installation")
 	}
 
-	codegen := exec.Command("npx", "playwright", "codegen")
+	wd, _ := os.Getwd()
+	outPath := path.Join(wd, "generated/tests.spec.ts")
+
+	err = os.MkdirAll(path.Join(wd, "generated"), os.ModePerm)
+	if err != nil {
+		log.Fatal("could not create dir: ", err)
+	}
+
+	_, err = os.OpenFile(outPath, os.O_RDONLY|os.O_CREATE, 0777)
+	if err != nil {
+		log.Fatal("could not create file: ", err)
+	}
+
+	fmt.Println(outPath)
+
+	codegen := exec.Command("npx", "playwright", "codegen", "-o", outPath)
 	err = codegen.Run()
 	if err != nil {
-		log.Error("Make sure npx and playwright are installed globally")
+		log.Info("Make sure playwright and its browsers are installed globally")
 		log.Fatal("could not start codegen: ", err)
 	}
 }
