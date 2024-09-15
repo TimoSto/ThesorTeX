31211ca3f7707880a28baa8596bbc3e40b80e906
Timo Stovermann <tsto@d-velop.de>
2024-09-15 13:32:44 +0200

    tools/generator: add dedicated playwright package

--- tools/generatePlaywrightA11yTree/recordA11yTree.sh
+++ tools/generatePlaywrightA11yTree/recordA11yTree.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Step 1: generate playwright test to go to the page and click/... as needed for the test
-cd ../../tests/playwright
+cd ./packages/playwright
 
 pnpm run codegen
 
-cd ../../tools/generatePlaywrightA11yTree
+cd ../../
 
 # Step 2: at the end of the test, generate the a11y tree and write it to a file
 go run cmd/generateGetTree/main.go
